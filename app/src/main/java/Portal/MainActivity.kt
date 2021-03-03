dee2b98aca4392ea8b9dfa08e992a355a9e10edd
Dominik277 <dominikdoric277@gmail.com>
2021-03-03 12:50:48 +0100

    Dodano dosta toga, neke stvari u MainActivity.kt a i razni dependency.

--- app/src/main/java/Portal/MainActivity.kt
+++ app/src/main/java/Portal/MainActivity.kt
@@ -3,10 +3,29 @@ package Portal
 import Portal.a257.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var appBarConfiguration: AppBarConfiguration
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        navController = findNavController(R.id.main_nav_host)
+
+        appBarConfiguration = AppBarConfiguration.Builder(R.id.homeFragment,R.id.accountsFragment,
+        R.id.dashboardFragment,R.id.settingsFragment)
+            .setDrawerLayout(main_drawer_layout)
+            .build()
+
+        setSupportActionBar(main_toolbar)
+
     }
 }
+
