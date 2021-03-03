b1a77e7a902969c7c00115c6b1e4bcd9646839a4
Dominik277 <dominikdoric277@gmail.com>
2021-03-03 12:55:23 +0100

    Dodane neke od metoda koje ce mi biti potrebne jedino ih jos implementirati.

--- app/src/main/java/Portal/MainActivity.kt
+++ app/src/main/java/Portal/MainActivity.kt
@@ -3,9 +3,12 @@ package Portal
 import Portal.a257.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -26,6 +29,46 @@ class MainActivity : AppCompatActivity() {
 
         setSupportActionBar(main_toolbar)
 
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        visibilityNavElements(navController)
+
     }
+
+    private fun visibilityNavElements(navController: NavController) {
+
+    }
+
+    private fun hideBothNavigation(){
+
+    }
+
+    private fun showBothNavigation(){
+
+    }
+
+    private fun setupNavControl(){
+
+    }
+
+    fun exitApp(){
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController,appBarConfiguration)
+    }
+
+    override fun onBackPressed() {
+        when{
+            main_drawer_layout.isDrawerOpen(GravityCompat.START) -> {
+                main_drawer_layout.closeDrawer(GravityCompat.START)
+            }
+            else -> {
+                super.onBackPressed()
+            }
+        }
+    }
+
 }
 
