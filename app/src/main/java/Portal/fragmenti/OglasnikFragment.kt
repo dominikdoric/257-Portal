b20ac207a6fd352b194ec9444c81a9e1afec715e
Dominik277 <dominikdoric277@gmail.com>
2021-03-04 13:30:35 +0100

    Povezani svi fragmenti s njihovim pripadajucim layout resursima.

--- app/src/main/java/Portal/fragmenti/OglasnikFragment.kt
+++ app/src/main/java/Portal/fragmenti/OglasnikFragment.kt
@@ -1,9 +1,18 @@
 package Portal.fragmenti
 
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
 class OglasnikFragment: Fragment() {
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.oglasnik_fragment,container,false)
 
+        return view
+    }
 
 }
