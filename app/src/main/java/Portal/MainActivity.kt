7e3ae2b60ba272c911da5410d4ddc0a438c6a6f3
Dominik277 <dominikdoric277@gmail.com>
2021-03-05 11:44:50 +0100

    Unešeni navigation drawer i bottom navigation drawer u activity_main.xml.

--- app/src/main/java/Portal/MainActivity.kt
+++ app/src/main/java/Portal/MainActivity.kt
@@ -23,7 +23,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setSupportActionBar(main_toolbar)
+        setSupportActionBar(toolbar)
 
 
     }
