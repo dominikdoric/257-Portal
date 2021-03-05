b0dd05a26c6d98d9537ada24f8d930026c852b3a
Dominik277 <dominikdoric277@gmail.com>
2021-03-05 09:56:52 +0100

    Spojene klase od fragmenata s njihovim layout resoursima i dodane destinacije u navigacijski graf.

--- app/src/main/java/Portal/dodajNovi/DodajNoviJaNovinarFragment.kt
+++ app/src/main/java/Portal/dodajNovi/DodajNoviJaNovinarFragment.kt
@@ -1,4 +1,19 @@
 package Portal.dodajNovi
 
-class DodajNoviJaNovinarFragment {
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DodajNoviJaNovinarFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.dodaj_novi_ja_novinar_fragment,container,false)
+
+
+        return view
+    }
+
 }
--- app/src/main/java/Portal/uredi-obrisi/UrediObrisiJaNovinarFragment.kt
+++ app/src/main/java/Portal/uredi-obrisi/UrediObrisiJaNovinarFragment.kt
@@ -1,4 +1,19 @@
 package Portal.`uredi-obrisi`
 
-class UrediObrisiJaNovinarFragment {
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class UrediObrisiJaNovinarFragment: Fragment() {
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view = inflater.inflate(R.layout.uredi_obrisi_ja_novinar_fragment,container,false)
+
+
+        return view
+    }
+
 }
