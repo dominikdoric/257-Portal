670aa68692cc7302f2bb9fb9dc509cfcd7174f54
Dominik277 <dominikdoric277@gmail.com>
2021-03-13 20:28:39 +0100

    Dodano jos par fragmenata koji ce mi trebati.

--- app/src/main/java/Portal/pokus/PokusActivity.kt
+++ app/src/main/java/Portal/pokus/PokusActivity.kt
@@ -3,6 +3,9 @@ package Portal.pokus
 import Portal.a257.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_pokus.*
@@ -18,6 +21,14 @@ class PokusActivity : AppCompatActivity() {
 
     }
 
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu_pokus,menu)
+        return true
+    }
 
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
 
 }
