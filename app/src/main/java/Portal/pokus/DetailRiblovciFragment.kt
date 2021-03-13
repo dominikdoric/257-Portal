a001eb1b8e52bd92537f8abcb6f708b3a48b7ba0
Dominik277 <dominikdoric277@gmail.com>
2021-03-13 20:33:09 +0100

    Spojeni novi fragmenti sa layout fileovima.

--- app/src/main/java/Portal/pokus/DetailRiblovciFragment.kt
+++ app/src/main/java/Portal/pokus/DetailRiblovciFragment.kt
@@ -1,4 +1,22 @@
 package Portal.pokus
 
-class DetailRiblovciFragment {
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DetailRiblovciFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.pokus_detail_ulovljene_ribe_fragment,container,false)
+
+        return view
+    }
+
 }
