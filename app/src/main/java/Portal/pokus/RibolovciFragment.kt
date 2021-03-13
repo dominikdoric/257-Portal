cb2ca19e4fde4722be1e4b126232cc69639a3d55
Dominik277 <dominikdoric277@gmail.com>
2021-03-13 19:55:11 +0100

    Spojeni fragmenti sa njihovim layout resursima i dodan izgled za navigation drawer.

--- app/src/main/java/Portal/pokus/RibolovciFragment.kt
+++ app/src/main/java/Portal/pokus/RibolovciFragment.kt
@@ -1,4 +1,23 @@
 package Portal.pokus
 
-class RibolovciFragment {
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class RibolovciFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.pokus_ribolovci,container,false)
+
+
+        return view
+    }
+
 }
--- app/src/main/java/Portal/pokus/UlovljeneRibeFragment.kt
+++ app/src/main/java/Portal/pokus/UlovljeneRibeFragment.kt
@@ -1,4 +1,22 @@
 package Portal.pokus
 
-class UlovljeneRibeFragment {
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class UlovljeneRibeFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.pokus_ulovljene_ribe,container,false)
+
+        return view
+    }
+
 }
