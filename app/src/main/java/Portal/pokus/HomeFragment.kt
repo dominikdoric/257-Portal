1f3fd5685857c167c44f102587b3552db50e880b
Dominik277 <dominikdoric277@gmail.com>
2021-03-14 13:00:53 +0100

    Kreiran vizualni dio za welcome_fragment.xml.

--- app/src/main/java/Portal/pokus/HomeFragment.kt
+++ app/src/main/java/Portal/pokus/HomeFragment.kt
@@ -1,10 +1,20 @@
 package Portal.pokus
 
 import Portal.a257.R
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.home_fragment.*
 
 class HomeFragment: Fragment(R.layout.home_fragment) {
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-
+        button_login.setOnClickListener {
+            val action = HomeFragmentDirections.actionHomeFragmentToLoginFragment()
+            findNavController().navigate(action)
+        }
+    }
 }
---
+++ app/src/main/java/Portal/pokus/WelcomeFragment.kt
@@ -0,0 +1,10 @@
+package Portal.pokus
+
+import Portal.a257.R
+import androidx.fragment.app.Fragment
+
+class WelcomeFragment: Fragment(R.layout.welcome_fragment) {
+
+
+
+}
