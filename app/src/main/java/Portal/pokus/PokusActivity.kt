f139220c48a596259cdd77701890bbb66e466760
Dominik277 <dominikdoric277@gmail.com>
2021-03-14 13:43:52 +0100

    Spojen toolbar sa navigation componentom.

--- app/src/main/java/Portal/pokus/PokusActivity.kt
+++ app/src/main/java/Portal/pokus/PokusActivity.kt
@@ -7,16 +7,33 @@ import android.view.Menu
 import android.view.MenuInflater
 import android.view.MenuItem
 import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_pokus.*
 
 class PokusActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_pokus)
         setSupportActionBar(toolbarPokus)
 
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.frameLayout_hostPokus) as NavHostFragment
+        navController = navHostFragment.findNavController()
+        setupActionBarWithNavController(navController)
+
 
 
     }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
+    }
+
 }
--- app/src/main/java/Portal/pokus/WelcomeFragment.kt
+++ app/src/main/java/Portal/pokus/WelcomeFragment.kt
@@ -4,6 +4,7 @@ import Portal.a257.R
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
 import kotlinx.android.synthetic.main.welcome_fragment.*
 
@@ -16,6 +17,11 @@ class WelcomeFragment: Fragment(R.layout.welcome_fragment) {
 
         textView_username.text = args.username
         textView_password.text = args.password
+
+        button_ok.setOnClickListener{
+            val action = WelcomeFragmentDirections.actionWelcomeFragmentToHomeFragment()
+            findNavController().navigate(action)
+        }
     }
 
 }
