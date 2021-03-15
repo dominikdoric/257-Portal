5c94d035cf29c48b51fe6ed9eeb9365858f4dca2
Dominik277 <dominikdoric277@gmail.com>
2021-03-15 13:45:22 +0100

    Implementirano da se ne vidi back button gumb kada otvorimo top level destinacije.

--- app/src/main/java/Portal/MainActivity.kt
+++ app/src/main/java/Portal/MainActivity.kt
@@ -14,14 +14,17 @@ import androidx.core.view.GravityCompat
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.onNavDestinationSelected
 import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import kotlinx.android.synthetic.main.activity_main.*
 import java.util.*
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,8 +32,13 @@ class MainActivity : AppCompatActivity() {
 
         val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_main) as NavHostFragment
         navController = navHostFragment.findNavController()
+        appBarConfiguration = AppBarConfiguration(
+            setOf(R.id.naslovnicaBottomNav,R.id.vrijemeBottomNav,R.id.infoBottomNav,R.id.kontaktBottomNav)
+        )
         setSupportActionBar(toolbar)
-        setupActionBarWithNavController(navController)
+        setupActionBarWithNavController(navController,appBarConfiguration)
+
+        bottom_navigation.setupWithNavController(navController)
 
     }
 
