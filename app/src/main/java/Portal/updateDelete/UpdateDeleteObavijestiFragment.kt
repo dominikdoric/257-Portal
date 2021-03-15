9ece618f766bd710f84ae3c65d09a1669fc5a4d0
Dominik277 <dominikdoric277@gmail.com>
2021-03-15 19:41:03 +0100

    Kreiran fragment za update i delete item-a iz baze podataka.

---
+++ app/src/main/java/Portal/updateDelete/UpdateDeleteObavijestiFragment.kt
@@ -0,0 +1,10 @@
+package Portal.updateDelete
+
+import Portal.a257.R
+import androidx.fragment.app.Fragment
+
+class UpdateDeleteObavijestiFragment: Fragment(R.layout.update_delete_obavijesti_fragment) {
+
+
+
+}
