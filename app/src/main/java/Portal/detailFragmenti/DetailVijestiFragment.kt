e95d9e8372518cf5d6a686a130bb6a772515e71f
Dominik277 <dominikdoric277@gmail.com>
2021-03-16 12:34:23 +0100

    Dodani svi novi detail fragmenti i spojeni sa njihovim pripadajucim layout fileovima.

---
+++ app/src/main/java/Portal/detailFragmenti/DetailVijestiFragment.kt
@@ -0,0 +1,22 @@
+package Portal.detailFragmenti
+
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DetailVijestiFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.detail_vijesti_fragment,container,false)
+
+        return view
+    }
+
+}
