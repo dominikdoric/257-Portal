d80e0dfc857652f6691123cf636e53a4fae78a0c
Dominik277 <dominikdoric277@gmail.com>
2021-03-16 16:05:59 +0100

    Implementiran price_citatelja_fragment.xml i PriceCitateljaFragment.kt.

--- app/src/main/java/Portal/adapter/PriceCitateljaAdapter.kt
+++ app/src/main/java/Portal/adapter/PriceCitateljaAdapter.kt
@@ -1,4 +1,44 @@
 package Portal.adapter
 
-class PriceCitateljaAdapter {
+import Portal.a257.R
+import Portal.database.table.PriceCitateljaTable
+import Portal.database.table.SportTable
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.jedan_red_price_citatelja.view.*
+
+class PriceCitateljaAdapter: RecyclerView.Adapter<PriceCitateljaAdapter.ViewHolder>() {
+
+    private var priceCitateljaList = emptyList<PriceCitateljaTable>()
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): PriceCitateljaAdapter.ViewHolder {
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.jedan_red_price_citatelja, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: PriceCitateljaAdapter.ViewHolder, position: Int) {
+        val currentItem = priceCitateljaList[position]
+        holder.itemView.textViewPricaCitateljaNaslov.text = currentItem.priceCitateljaNaslov
+        holder.itemView.textViewPricaCitateljaVrijeme.text = currentItem.priceCitateljaVrijeme
+    }
+
+    override fun getItemCount(): Int {
+        return priceCitateljaList.size
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    fun setData(priceCitatelja: List<PriceCitateljaTable>) {
+        this.priceCitateljaList = priceCitatelja
+        notifyDataSetChanged()
+    }
+
 }
--- app/src/main/java/Portal/fragmenti/PriceCitateljaFragment.kt
+++ app/src/main/java/Portal/fragmenti/PriceCitateljaFragment.kt
@@ -1,22 +1,51 @@
 package Portal.fragmenti
 
 import Portal.a257.R
+import Portal.adapter.PriceCitateljaAdapter
+import Portal.viewModel.PriceCitateljaViewModel
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.fragment.findNavController
+import androidx.recyclerview.widget.DividerItemDecoration
+import androidx.recyclerview.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.price_citatelja_fragment.*
+import kotlinx.android.synthetic.main.price_citatelja_fragment.view.*
 
 class PriceCitateljaFragment: Fragment() {
+
+    private lateinit var mPriceCitateljaViewModel: PriceCitateljaViewModel
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.price_citatelja_fragment,container,false)
 
+        //RecyclerView
+        val adapter = PriceCitateljaAdapter()
+        val recyclerPriceCitatelja = view.recyclerViewPriceCitatelja
+        recyclerPriceCitatelja.adapter = adapter
+        recyclerPriceCitatelja.layoutManager = LinearLayoutManager(requireContext())
+
+        //ViewModel
+        mPriceCitateljaViewModel = ViewModelProvider(this).get(PriceCitateljaViewModel::class.java)
+        mPriceCitateljaViewModel.readAllDataPriceCitatelja.observe(viewLifecycleOwner, Observer { priceCitatelja ->
+            adapter.setData(priceCitatelja)
+        })
 
         return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        recyclerViewPriceCitatelja.addItemDecoration(
+            DividerItemDecoration(recyclerViewPriceCitatelja.context,DividerItemDecoration.VERTICAL)
+        )
     }
 
 }
