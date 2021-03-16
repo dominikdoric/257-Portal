1d2cfcb29ba578f4ef35e0ec46901df27b762c88
Dominik277 <dominikdoric277@gmail.com>
2021-03-16 12:47:00 +0100

    Dodani vizualni dijelovi za detail fragmente.

--- app/src/main/java/Portal/detailFragmenti/DetailOglasnikFragment.kt
+++ app/src/main/java/Portal/detailFragmenti/DetailOglasnikFragment.kt
@@ -1,13 +1,19 @@
 package Portal.detailFragmenti
 
 import Portal.a257.R
+import Portal.viewModel.OglasnikViewModel
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.fragment.navArgs
 
 class DetailOglasnikFragment: Fragment() {
+
+    private val args by navArgs<DetailOglasnikFragmentArgs>()
+    private lateinit var mOglasnikViewModel: OglasnikViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -15,6 +21,9 @@ class DetailOglasnikFragment: Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.detail_oglasnik_fragment,container,false)
+
+        mOglasnikViewModel = ViewModelProvider(this).get(OglasnikViewModel::class.java)
+
 
         return view
     }
