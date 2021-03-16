d1927ef9acb9d46e03431e66d1f6b320ed3a04af
Dominik277 <dominikdoric277@gmail.com>
2021-03-16 12:38:02 +0100

    Dodani novi fragmenti u navigacijski graf i njihove akcije.

--- app/src/main/java/Portal/detailFragmenti/DetailZabavaFragment.kt
+++ app/src/main/java/Portal/detailFragmenti/DetailZabavaFragment.kt
@@ -7,7 +7,7 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class DetailFragmentZabava: Fragment() {
+class DetailZabavaFragment: Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater,
