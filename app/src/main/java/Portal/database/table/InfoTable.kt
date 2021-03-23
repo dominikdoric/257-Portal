0ad67e6659607b0ff03bcfa502536b83c0a0cf7d
Dominik277 <dominikdoric277@gmail.com>
2021-03-23 23:14:08 +0100

    Kreirana tablica unutar baze podataka za info.

--- app/src/main/java/Portal/database/table/InfoTable.kt
+++ app/src/main/java/Portal/database/table/InfoTable.kt
@@ -1,4 +1,23 @@
 package Portal.database.table
 
-class InfoTable {
-}
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+
+@Parcelize
+@Entity(tableName = "info")
+data class InfoTable (
+
+    @PrimaryKey(autoGenerate = true)
+    var id: Int,
+
+    @ColumnInfo(name = "ime")
+    val ime: String,
+
+    @ColumnInfo(name = "prezime")
+    val prezime: String
+
+    ): Parcelable
