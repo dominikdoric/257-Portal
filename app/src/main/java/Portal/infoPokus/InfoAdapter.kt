32ba38d7f53cb493b883842dffa349b856a4dbde
Dominik277 <dominikdoric277@gmail.com>
2021-03-23 23:27:09 +0100

    Dodan kostur za adapter za recyclerview.

--- app/src/main/java/Portal/infoPokus/InfoAdapter.kt
+++ app/src/main/java/Portal/infoPokus/InfoAdapter.kt
@@ -1,4 +1,33 @@
 package Portal.infoPokus
 
-class InfoAdapter {
+import Portal.database.table.InfoTable
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class InfoAdapter: RecyclerView.Adapter<InfoAdapter.ViewHolder>() {
+
+    private var infoList = emptyList<InfoTable>()
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): InfoAdapter.ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: InfoAdapter.ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    fun setData(infoTable: List<InfoTable>){
+        this.infoList = infoTable
+        notifyDataSetChanged()
+    }
+
 }
