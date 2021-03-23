2b2f6ea7ec85892725e853d8740290a878629cf4
Dominik277 <dominikdoric277@gmail.com>
2021-03-23 19:50:24 +0100

    Kreiran modul baze podataka koji je singleton te ce se samo ta jedna instanca koristiti unutar aplikacije.

--- app/src/main/java/Portal/di/DatabaseModule.kt
+++ app/src/main/java/Portal/di/DatabaseModule.kt
@@ -1,5 +1,35 @@
 package Portal.di
 
+import Portal.a257.R
+import Portal.database.Portal257Database
+import Portal.database.dao.SportDao
+import android.app.Application
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
+@Module
+@InstallIn(SingletonComponent::class)
 object DatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideAppDatabase(application: Application): Portal257Database {
+        return Room.databaseBuilder(
+            application,
+            Portal257Database::class.java,
+            application.getString(R.string.database)
+        ).fallbackToDestructiveMigration()
+            .build()
+    }
+
+    @Provides
+    @Singleton
+    fun provideSportDao(portal257Database: Portal257Database): SportDao{
+        return portal257Database.sportDao()
+    }
+
 }
