9541976f1723315fade56cd3f9bd009448652700
Dominik277 <dominikdoric277@gmail.com>
2021-03-23 21:20:13 +0100

    Malo promjenjena implementacija funkcija u DatabaseModule.kt.

--- app/src/main/java/Portal/di/DatabaseModule.kt
+++ app/src/main/java/Portal/di/DatabaseModule.kt
@@ -4,10 +4,12 @@ import Portal.a257.R
 import Portal.database.Portal257Database
 import Portal.database.dao.SportDao
 import android.app.Application
+import android.content.Context
 import androidx.room.Room
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
 
@@ -17,19 +19,16 @@ object DatabaseModule {
 
     @Provides
     @Singleton
-    fun provideAppDatabase(application: Application): Portal257Database {
-        return Room.databaseBuilder(
-            application,
-            Portal257Database::class.java,
-            application.getString(R.string.database)
-        ).fallbackToDestructiveMigration()
-            .build()
-    }
+    fun provideDatabase(
+        @ApplicationContext app: Context
+    ) = Room.databaseBuilder(
+        app,
+        Portal257Database::class.java,
+        "portal_257"
+    ).build()
 
     @Provides
     @Singleton
-    fun provideSportDao(portal257Database: Portal257Database): SportDao{
-        return portal257Database.sportDao()
-    }
+    fun provideSportDao(portal257Database: Portal257Database) = portal257Database.sportDao()
 
 }
