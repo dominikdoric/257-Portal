6776077c17864d2c516a4e5645c68a8a922a3ec6
Dominik277 <dominikdoric277@gmail.com>
2021-03-24 13:06:31 +0100

    Dodan UpdateDeleteInfoFragment.kt u navigacijski graf.

--- app/src/main/java/Portal/infoPokus/UpdateDeleteInfoFragment.kt
+++ app/src/main/java/Portal/infoPokus/UpdateDeleteInfoFragment.kt
@@ -6,8 +6,16 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.navArgs
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.update_delete_info_fragment.view.*
 
+@AndroidEntryPoint
 class UpdateDeleteInfoFragment: Fragment() {
+
+    private val args by navArgs<UpdateDeleteInfoFragmentArgs>()
+    private val infoViewModel: InfoViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -16,7 +24,23 @@ class UpdateDeleteInfoFragment: Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.update_delete_info_fragment,container,false)
 
+        view.gumbUpdateInfo.setOnClickListener {
+            updateItemInfo()
+        }
+
+        view.gumbDeleteInfo.setOnClickListener {
+            deleteItemInfo()
+        }
+
         return view
+    }
+
+    private fun deleteItemInfo() {
+        TODO("Not yet implemented")
+    }
+
+    private fun updateItemInfo() {
+        TODO("Not yet implemented")
     }
 
 }
