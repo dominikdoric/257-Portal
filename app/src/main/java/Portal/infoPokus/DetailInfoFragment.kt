c90be54feb08e58de2bef5305b17678837fa2209
Dominik277 <dominikdoric277@gmail.com>
2021-03-24 14:35:51 +0100

    Dodan DetailInfoFragment.kt te njegov layout.

---
+++ app/src/main/java/Portal/infoPokus/DetailInfoFragment.kt
@@ -0,0 +1,22 @@
+package Portal.infoPokus
+
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DetailInfoFragment: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.detail_info_fragment,container,false)
+
+        return view
+    }
+
+}
