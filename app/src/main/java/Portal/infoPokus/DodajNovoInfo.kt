9cd08ccdce1aed7625ddb57431311d6f10d3fd1f
Dominik277 <dominikdoric277@gmail.com>
2021-03-24 10:12:36 +0100

    Kreiran vizualni dio za novi info fragment.

---
+++ app/src/main/java/Portal/infoPokus/DodajNovoInfo.kt
@@ -0,0 +1,22 @@
+package Portal.infoPokus
+
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+class DodajNovoInfo: Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.dodaj_novo_info_fragment,container,false)
+
+        return view
+    }
+
+}
