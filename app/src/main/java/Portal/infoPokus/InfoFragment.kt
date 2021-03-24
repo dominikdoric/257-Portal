8d6018abd5a6d8215e4d50ad3a5c8331d9dae770
Dominik277 <dominikdoric277@gmail.com>
2021-03-24 11:28:10 +0100

    Kreirana metoda za popunjavanje recyclerview-a.

--- app/src/main/java/Portal/infoPokus/InfoFragment.kt
+++ app/src/main/java/Portal/infoPokus/InfoFragment.kt
@@ -7,12 +7,15 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.info_fragment.*
 
 @AndroidEntryPoint
 class InfoFragment : Fragment() {
 
     private val infoViewModel: InfoViewModel by viewModels()
+    private lateinit var infoAdapter: InfoAdapter
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -21,6 +24,17 @@ class InfoFragment : Fragment() {
     ): View? {
         val view = inflater.inflate(R.layout.info_fragment, container, false)
 
+        setupRecyclerView()
+
         return view
     }
+
+    private fun setupRecyclerView() = recyclerViewInfo.apply {
+        infoAdapter = InfoAdapter()
+        adapter = infoAdapter
+        layoutManager = LinearLayoutManager(requireContext())
+    }
+
 }
+
+
