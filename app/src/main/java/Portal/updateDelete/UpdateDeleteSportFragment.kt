c3b380524ba3d828ef1cf5f45b2cc8cc12fc57e4
Dominik277 <dominikdoric277@gmail.com>
2021-03-25 18:22:53 +0100

    Pokus 3.

--- app/src/main/java/Portal/updateDelete/UpdateDeleteSportFragment.kt
+++ app/src/main/java/Portal/updateDelete/UpdateDeleteSportFragment.kt
@@ -29,7 +29,7 @@ class UpdateDeleteSportFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup? ,
-        savedInstanceState: Bundle?
+        savedInstanceState:  Bundle?
     ): View? {
         val view  =  inflater.inflate(R.layout.update_delete_sport_fragment, container, false)
 
