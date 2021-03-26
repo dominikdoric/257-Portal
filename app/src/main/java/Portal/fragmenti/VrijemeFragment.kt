36ddc18d72247790467b2fb86d28230aafe93205
Dominik277 <dominikdoric277@gmail.com>
2021-03-26 10:49:46 +0100

    Dodan background color u vrijeme_fragment.xml.

--- app/src/main/java/Portal/fragmenti/VrijemeFragment.kt
+++ app/src/main/java/Portal/fragmenti/VrijemeFragment.kt
@@ -7,10 +7,14 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 
-class VrijemeFragment: Fragment() {
+class VrijemeFragment : Fragment() {
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view = inflater.inflate(R.layout.vrijeme_fragment,container,false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.vrijeme_fragment, container, false)
 
         return view
     }
