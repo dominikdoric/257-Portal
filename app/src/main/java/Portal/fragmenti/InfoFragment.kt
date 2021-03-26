e88183938e77907cc0ed1509a4dd68aa6f2ba082
Dominik277 <dominikdoric277@gmail.com>
2021-03-26 10:40:50 +0100

    Izbrisano sve nepotrebno.

---
+++ app/src/main/java/Portal/fragmenti/InfoFragment.kt
@@ -0,0 +1,25 @@
+package Portal.fragmenti
+
+import Portal.a257.R
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class InfoFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.info_fragment, container, false)
+
+        return view
+    }
+}
+
+
