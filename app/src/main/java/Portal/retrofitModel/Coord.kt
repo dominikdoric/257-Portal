6884b1a36aff40e345fde4c0577474cbc241855a
Dominik277 <dominikdoric277@gmail.com>
2021-03-27 10:22:53 +0100

    Kreirane klase Coord.kt i Main.kt.

--- app/src/main/java/Portal/retrofitModel/Coord.kt
+++ app/src/main/java/Portal/retrofitModel/Coord.kt
@@ -1,5 +1,14 @@
 package Portal.retrofitModel
 
-data class Coord (
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
+data class Coord (
+        @SerializedName("lat")
+        @Expose
+        val lat: Double? = null,
+
+        @SerializedName("lon")
+        @Expose
+        val lon: Double? = null
         )
