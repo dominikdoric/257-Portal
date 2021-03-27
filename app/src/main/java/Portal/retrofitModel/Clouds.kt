3ee5be4c43d4548a786e9710dc176435e4921ed8
Dominik277 <dominikdoric277@gmail.com>
2021-03-27 10:21:43 +0100

    Kreirane model klase City.kt i Clouds.kt.

--- app/src/main/java/Portal/retrofitModel/Clouds.kt
+++ app/src/main/java/Portal/retrofitModel/Clouds.kt
@@ -1,5 +1,10 @@
 package Portal.retrofitModel
 
-data class Clouds (
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
 
+data class Clouds (
+        @SerializedName("all")
+        @Expose
+        val all: Int? = null
         )
