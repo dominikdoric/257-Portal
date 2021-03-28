df6bd8e6e82e7e767aa007bfca9434700c0b608f
Dominik277 <dominikdoric277@gmail.com>
2021-03-28 22:25:53 +0200

    Dodano jos par stvari.

--- app/src/main/java/Portal/retrofit/repository/Repository.kt
+++ app/src/main/java/Portal/retrofit/repository/Repository.kt
@@ -1,4 +1,12 @@
 package Portal.retrofit.repository
 
+import Portal.retrofit.api.RetrofitInstance
+import Portal.retrofit.model.PostModel
+
 class Repository {
+
+    suspend fun getPost(): PostModel{
+        return RetrofitInstance.api.getPost()
+    }
+
 }
---
+++ app/src/main/java/Portal/retrofit/viewModel/MainViewModelFactory.kt
@@ -0,0 +1,15 @@
+package Portal.retrofit.viewModel
+
+import Portal.retrofit.repository.Repository
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+
+class MainViewModelFactory(
+    private val repository: Repository
+) : ViewModelProvider.Factory {
+    override fun <T : ViewModel?> create(modelClass: Class<T>): T {
+        return MainViewModel(repository) as T
+    }
+
+
+}
