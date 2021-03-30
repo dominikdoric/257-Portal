f01ec671227321f48f91f779ab5b9df422437a2f
Dominik277 <dominikdoric277@gmail.com>
2021-03-30 23:18:08 +0200

    Unesen binding u VijestiFragment.kt.

--- app/src/main/java/Portal/fragmenti/fragmenti/OglasnikFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/OglasnikFragment.kt
@@ -3,22 +3,18 @@ package Portal.fragmenti.fragmenti
 import Portal.a257.R
 import Portal.adapter.OglasnikAdapter
 import Portal.viewModel.OglasnikViewModel
-import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.oglasnik_fragment.*
 import kotlinx.android.synthetic.main.oglasnik_fragment.view.*
-import kotlinx.android.synthetic.main.zabava_fragment.*
 
 @AndroidEntryPoint
 class OglasnikFragment : Fragment() {
--- app/src/main/java/Portal/fragmenti/fragmenti/VijestiFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/VijestiFragment.kt
@@ -1,40 +1,37 @@
 package Portal.fragmenti.fragmenti
 
 import Portal.a257.R
+import Portal.a257.databinding.VijestiFragmentBinding
 import Portal.adapter.VijestiAdapter
 import Portal.viewModel.VijestiViewModel
-import android.app.AlertDialog
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.vijesti_fragment.*
-import kotlinx.android.synthetic.main.vijesti_fragment.view.*
-import kotlinx.android.synthetic.main.zabava_fragment.*
 
 @AndroidEntryPoint
-class VijestiFragment : Fragment() {
+class VijestiFragment : Fragment(R.layout.vijesti_fragment) {
 
     private val mVijestiViewModel: VijestiViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.vijesti_fragment, container, false)
+    private lateinit var binding: VijestiFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = VijestiFragmentBinding.bind(view)
+
+        binding.recyclerViewVijesti.addItemDecoration(
+            DividerItemDecoration
+                (binding.recyclerViewVijesti.context, DividerItemDecoration.VERTICAL)
+        )
 
         //RecyclerView
         val adapter = VijestiAdapter()
-        val recyclerVijesti = view.recyclerViewVijesti
+        val recyclerVijesti = binding.recyclerViewVijesti
         recyclerVijesti.adapter = adapter
         recyclerVijesti.layoutManager = LinearLayoutManager(requireContext())
 
@@ -43,15 +40,5 @@ class VijestiFragment : Fragment() {
             adapter.setData(vijesti)
         })
 
-        return view
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-        recyclerViewVijesti.addItemDecoration(
-            DividerItemDecoration
-                (recyclerViewVijesti.context, DividerItemDecoration.VERTICAL)
-        )
     }
 }
