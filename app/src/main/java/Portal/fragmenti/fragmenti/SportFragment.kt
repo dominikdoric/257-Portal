43cc2b560a4bd4731b8b04032c44f5beaf7ca8f4
Dominik277 <dominikdoric277@gmail.com>
2021-03-30 22:55:58 +0200

    Dodan dependecy za view binding u projekt.

--- app/src/main/java/Portal/fragmenti/fragmenti/SportFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/SportFragment.kt
@@ -3,24 +3,18 @@ package Portal.fragmenti.fragmenti
 import Portal.a257.R
 import Portal.adapter.SportAdapter
 import Portal.viewModel.SportViewModel
-import android.app.AlertDialog
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
-import androidx.navigation.fragment.findNavController
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.dodaj_novo_sport_fragment.*
 import kotlinx.android.synthetic.main.sport_fragment.*
 import kotlinx.android.synthetic.main.sport_fragment.view.*
-import kotlinx.android.synthetic.main.zabava_fragment.*
 
 @AndroidEntryPoint
 class SportFragment : Fragment() {
