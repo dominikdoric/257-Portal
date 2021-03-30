f19c06a600460423f837474930d97473d5dcb243
Dominik277 <dominikdoric277@gmail.com>
2021-03-30 16:09:44 +0200

    Kreirana data klasa do kraja.

--- app/src/main/java/Portal/retrofit/model/WeatherModel.kt
+++ app/src/main/java/Portal/retrofit/model/WeatherModel.kt
@@ -89,4 +89,41 @@ data class WeatherModel(
         val humidity: Int
     )
 
+    data class Wind(
+
+        @SerializedName("speed")
+        val speed: Double,
+
+        @SerializedName("deg")
+        val deg: Int,
+
+        @SerializedName("gust")
+        val gust: Double
+    )
+
+    data class Sys(
+
+        @SerializedName("type")
+        val type: Int,
+
+        @SerializedName("id")
+        val id: Long,
+
+        @SerializedName("country")
+        val country: String,
+
+        @SerializedName("sunrise")
+        val sunrise: Long,
+
+        @SerializedName("sunset")
+        val sunset: Long
+    )
+
+    data class Clouds(
+
+        @SerializedName("all")
+        val all: Int
+
+    )
+
 }
