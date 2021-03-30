8ba9cceab80b9225f24816885bb69a2ac040764e
Dominik277 <dominikdoric277@gmail.com>
2021-03-30 23:54:38 +0200

    Dodan view binding u DodajNovoZabava.kt.

--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoZabava.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoZabava.kt
@@ -1,6 +1,7 @@
 package Portal.fragmenti.dodajNovo
 
 import Portal.a257.R
+import Portal.a257.databinding.DodajNovoZabavaFragmentBinding
 import Portal.database.table.ZabavaTable
 import Portal.viewModel.ZabavaViewModel
 import android.annotation.SuppressLint
@@ -13,33 +14,31 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.dodaj_novo_zabava_fragment.*
-import kotlinx.android.synthetic.main.dodaj_novo_zabava_fragment.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
 @AndroidEntryPoint
-class DodajNovoZabava : Fragment() {
+class DodajNovoZabava : Fragment(R.layout.dodaj_novo_zabava_fragment) {
 
     private val mZabavaViewModel: ZabavaViewModel by viewModels()
 
-    override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        val view = inflater.inflate(R.layout.dodaj_novo_zabava_fragment, container, false)
+    private lateinit var binding: DodajNovoZabavaFragmentBinding
 
-        view.gumbSpremiZabavu.setOnClickListener {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DodajNovoZabavaFragmentBinding.bind(view)
+        binding.gumbSpremiZabavu.setOnClickListener {
             val action = DodajNovoZabavaDirections.actionMenuDodajNovuZabavaToZabavaNavDrawer()
             findNavController().navigate(action)
-            Toast.makeText(requireContext(),
+            Toast.makeText(
+                requireContext(),
                 "Vaš članak je zaprimljen te je poslan adminu na odobrenje.Hvala!",
-                Toast.LENGTH_LONG)
+                Toast.LENGTH_LONG
+            )
                 .show()
             insertDataToDatabase()
         }
-        return view
     }
 
     @SuppressLint("SimpleDateFormat")
@@ -47,12 +46,12 @@ class DodajNovoZabava : Fragment() {
         val sdf = SimpleDateFormat("dd.MM.yyyy. HH:mm")
         val currentDate = sdf.format(Date())
 
-        val noviNaslov = et_zabava_naslov.text.toString()
+        val noviNaslov = binding.et_zabava_naslov.text.toString()
         val novoVrijeme = currentDate
-        val noviClanak = et_zabava_clanak.text.toString()
+        val noviClanak = binding.et_zabava_clanak.text.toString()
         val novaSlika = R.drawable.jaksic
 
-        val zabava = ZabavaTable(0,noviNaslov, noviClanak, novoVrijeme, novaSlika)
+        val zabava = ZabavaTable(0, noviNaslov, noviClanak, novoVrijeme, novaSlika)
         mZabavaViewModel.addZabava(zabava)
     }
 }
