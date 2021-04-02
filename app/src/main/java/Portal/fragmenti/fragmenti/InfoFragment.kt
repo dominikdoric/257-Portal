8de6e802009080e7950739ba904176188902153f
Dominik277 <dominikdoric277@gmail.com>
2021-04-02 16:32:24 +0200

    Dodano jos par stvari za notification.

--- app/src/main/java/Portal/fragmenti/fragmenti/InfoFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/InfoFragment.kt
@@ -1,11 +1,16 @@
 package Portal.fragmenti.fragmenti
 
 import Portal.a257.R
+import Portal.a257.databinding.InfoFragmentBinding
 import android.app.NotificationChannel
 import android.app.NotificationManager
 import android.content.Context
 import android.graphics.Color
 import android.os.Build
+import android.os.Bundle
+import android.view.View
+import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import androidx.fragment.app.Fragment
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,6 +19,26 @@ class InfoFragment : Fragment(R.layout.info_fragment) {
 
     val CHANNEL_ID = "channelID"
     val CHANNEL_NAME = "channelName"
+    private lateinit var binding: InfoFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = InfoFragmentBinding.bind(view)
+        creatiNotificationChannel()
+
+        val notification = NotificationCompat.Builder(requireContext(),CHANNEL_ID)
+            .setContentTitle("Awesome notification")
+            .setContentText("This is the content text")
+            .setSmallIcon(R.drawable.ic_email)
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .build()
+
+        val notificationManager = NotificationManagerCompat.from(requireContext())
+
+        binding.buttonNoticifation.setOnClickListener {
+            notificationManager
+        }
+    }
 
     fun creatiNotificationChannel(){
         if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O){
