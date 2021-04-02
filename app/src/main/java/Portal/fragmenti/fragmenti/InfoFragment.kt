352242e2366dde9d44db23902dcf1e6d6d5df7a3
Dominik277 <dominikdoric277@gmail.com>
2021-04-02 16:43:50 +0200

    Implementirano da pritiskom na notification nas vrati tamo odakle je dosla obavijest.

--- app/src/main/java/Portal/fragmenti/fragmenti/InfoFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/InfoFragment.kt
@@ -4,7 +4,10 @@ import Portal.a257.R
 import Portal.a257.databinding.InfoFragmentBinding
 import android.app.NotificationChannel
 import android.app.NotificationManager
+import android.app.PendingIntent
+import android.app.TaskStackBuilder
 import android.content.Context
+import android.content.Intent
 import android.graphics.Color
 import android.os.Build
 import android.os.Bundle
@@ -19,6 +22,7 @@ class InfoFragment : Fragment(R.layout.info_fragment) {
 
     val CHANNEL_ID = "channelID"
     val CHANNEL_NAME = "channelName"
+    val NOTIFICATION_ID = 0
     private lateinit var binding: InfoFragmentBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -26,17 +30,24 @@ class InfoFragment : Fragment(R.layout.info_fragment) {
         binding = InfoFragmentBinding.bind(view)
         creatiNotificationChannel()
 
+        val intent = Intent(requireContext(),InfoFragment::class.java)
+        val pendingIntent = TaskStackBuilder.create(requireContext()).run {
+            addNextIntentWithParentStack(intent)
+            getPendingIntent(0,PendingIntent.FLAG_UPDATE_CURRENT)
+        }
+
         val notification = NotificationCompat.Builder(requireContext(),CHANNEL_ID)
             .setContentTitle("Awesome notification")
             .setContentText("This is the content text")
             .setSmallIcon(R.drawable.ic_email)
             .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+            .setContentIntent(pendingIntent)
             .build()
 
         val notificationManager = NotificationManagerCompat.from(requireContext())
 
         binding.buttonNoticifation.setOnClickListener {
-            notificationManager
+            notificationManager.notify(NOTIFICATION_ID,notification)
         }
     }
 
