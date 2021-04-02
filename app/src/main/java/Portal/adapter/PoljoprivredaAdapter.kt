25b09fd021e3bd8e63da6ad7b3decfec5f3f5a68
Dominik277 <dominikdoric277@gmail.com>
2021-04-03 00:06:03 +0200

    Implementiran view binding u PoljoprivredaAdapter.kt.

--- app/src/main/java/Portal/adapter/PoljoprivredaAdapter.kt
+++ app/src/main/java/Portal/adapter/PoljoprivredaAdapter.kt
@@ -1,38 +1,38 @@
 package Portal.adapter
 
-import Portal.a257.R
+import Portal.a257.databinding.JedanRedPoljoprivredaBinding
 import Portal.database.table.PoljoprivredaTable
 import Portal.fragmenti.fragmenti.PoljoprivredaFragmentDirections
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.jedan_red_poljoprivreda.view.*
 
-class PoljoprivredaAdapter() : RecyclerView.Adapter<PoljoprivredaAdapter.ViewHolder>() {
+class PoljoprivredaAdapter : RecyclerView.Adapter<PoljoprivredaAdapter.ViewHolder>() {
 
     private var poljoprivredaList = emptyList<PoljoprivredaTable>()
+    private lateinit var binding: JedanRedPoljoprivredaBinding
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PoljoprivredaAdapter.ViewHolder {
-        val view =
-            LayoutInflater.from(parent.context).inflate(R.layout.jedan_red_poljoprivreda, parent, false)
-        return ViewHolder(view)
+        val binding = JedanRedPoljoprivredaBinding
+            .inflate(LayoutInflater.from(parent.context),parent,false)
+        return ViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: PoljoprivredaAdapter.ViewHolder, position: Int) {
-        val currentItem = poljoprivredaList[position]
-        holder.itemView.textViewPoljoprivredaNaslov.text = currentItem.poljoprivredaNaslov
-        holder.itemView.textViewPoljoprivredaVrijeme.text = currentItem.poljoprivredaVrijeme
 
-        holder.itemView.cardViewPoljoprivreda.setOnLongClickListener {
+        val currentItem = poljoprivredaList[position]
+        binding.textViewPoljoprivredaNaslov.text = currentItem.poljoprivredaNaslov
+        binding.textViewPoljoprivredaVrijeme.text = currentItem.poljoprivredaVrijeme
+
+        binding.cardViewPoljoprivreda.setOnLongClickListener {
             val action =
                 PoljoprivredaFragmentDirections.actionPoljoprivredaNavDrawerToUpdateDeletePoljoprivredaFragment(currentItem)
             holder.itemView.findNavController().navigate(action)
             true
         }
 
-        holder.itemView.cardViewPoljoprivreda.setOnClickListener {
+        binding.cardViewPoljoprivreda.setOnClickListener {
             val action =
                 PoljoprivredaFragmentDirections.actionPoljoprivredaNavDrawerToDetailPoljoprivredaFragment(currentItem)
             holder.itemView.findNavController().navigate(action)
@@ -44,7 +44,7 @@ class PoljoprivredaAdapter() : RecyclerView.Adapter<PoljoprivredaAdapter.ViewHol
         return poljoprivredaList.size
     }
 
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    inner class ViewHolder(binding: JedanRedPoljoprivredaBinding) : RecyclerView.ViewHolder(binding.root) {
 
     }
 
