ba4b2674acc6ec355b8656c465df110986d53125
Dominik277 <dominikdoric277@gmail.com>
2021-04-03 00:28:48 +0200

    Kreirana logika za DetailPoljoprivredaFragment.kt i DodajNovoPoljoprivreda.kt.

--- app/src/main/java/Portal/fragmenti/detailFragmenti/DetailPoljoprivredaFragment.kt
+++ app/src/main/java/Portal/fragmenti/detailFragmenti/DetailPoljoprivredaFragment.kt
@@ -1,7 +1,23 @@
 package Portal.fragmenti.detailFragmenti
 
 import Portal.a257.R
+import Portal.a257.databinding.DetailPoljoprivredaFragmentBinding
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 
 class DetailPoljoprivredaFragment: Fragment(R.layout.detail_poljoprivreda_fragment) {
+
+    private val args by navArgs<DetailPoljoprivredaFragmentArgs>()
+    private lateinit var binding: DetailPoljoprivredaFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DetailPoljoprivredaFragmentBinding.bind(view)
+
+        binding.detailPoljoprivredaNaslov.text = args.detailPoljoprivredaArgs.poljoprivredaNaslov
+        binding.detailPoljoprivredaClanak.text = args.detailPoljoprivredaArgs.poljoprivredaClanak
+    }
+
 }
--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoPoljoprivreda.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoPoljoprivreda.kt
@@ -1,7 +1,56 @@
 package Portal.fragmenti.dodajNovo
 
 import Portal.a257.R
+import Portal.a257.databinding.DodajNovoPoljoprivredaFragmentBinding
+import Portal.database.table.PoljoprivredaTable
+import Portal.viewModel.PoljoprivredaViewModel
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
+import dagger.hilt.android.AndroidEntryPoint
+import java.text.SimpleDateFormat
+import java.util.*
 
+@AndroidEntryPoint
 class DodajNovoPoljoprivreda: Fragment(R.layout.dodaj_novo_poljoprivreda_fragment) {
+
+    private val mPoljoprivredaViewModel: PoljoprivredaViewModel by viewModels()
+    private lateinit var binding: DodajNovoPoljoprivredaFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = DodajNovoPoljoprivredaFragmentBinding.bind(view)
+
+        binding.gumbSpremiPoljoprivredu.setOnClickListener {
+            val action = DodajNovoPoljoprivredaDirections.actionMenuDodajNovuPoljoprivreduToPoljoprivredaNavDrawer()
+            findNavController().navigate(action)
+            Toast.makeText(
+                requireContext(),
+                "Vaš članak je zaprimljen te je poslan adminu na odobrenje.Hvala!",
+                Toast.LENGTH_LONG
+            )
+                .show()
+            insertDataToDatabase()
+        }
+
+    }
+
+    @SuppressLint("SimpleDateFormat")
+    private fun insertDataToDatabase() {
+        val sdf = SimpleDateFormat("dd.MM.yyyy. HH:mm")
+        val currentDate = sdf.format(Date())
+
+        val noviNaslov = binding.etPoljoprivredaNaslov.text.toString()
+        val novoVrijeme = currentDate
+        val noviClanak = binding.etPoljoprivredaClanak.text.toString()
+        val novaSlika = R.drawable.jaksic
+
+        val poljoprivreda = PoljoprivredaTable(0, noviNaslov, noviClanak, novoVrijeme, novaSlika)
+        mPoljoprivredaViewModel.addPoljoprivreda(poljoprivreda)
+    }
+
 }
