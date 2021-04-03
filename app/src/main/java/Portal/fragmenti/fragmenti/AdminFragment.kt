9555c2b94e24559d489ce3305eaacc6a2e732dce
Dominik277 <dominikdoric277@gmail.com>
2021-04-03 23:52:13 +0200

    Implementirana logika koja ispisuje error poruku u slucaju da je edit text ostao neispunjen.

--- app/src/main/java/Portal/fragmenti/fragmenti/AdminFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/AdminFragment.kt
@@ -1,10 +1,30 @@
 package Portal.fragmenti.fragmenti
 
 import Portal.a257.R
+import Portal.a257.databinding.AdminFragmentBinding
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
 
 class AdminFragment: Fragment(R.layout.admin_fragment) {
 
+    private lateinit var binding: AdminFragmentBinding
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = AdminFragmentBinding.bind(view)
+
+        binding.gumbAdminPrijaviSe.setOnClickListener {
+            if (binding.etAdminKorisnickoIme.text.toString().isEmpty()){
+                binding.etAdminKorisnickoIme.error = "Ovo polje je obavezno!"
+            }else if (binding.etAdminLozinka.text.toString().isEmpty()){
+                binding.etAdminLozinka.error = "Ovo polje je obavezno!"
+            }else if(binding.etAdminToken.text.toString().isEmpty()){
+                binding.etAdminToken.error = "Ovo polje je obavezno!"
+            }
+        }
+
+    }
 
 
 }
