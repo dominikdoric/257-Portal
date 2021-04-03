5e7911f4b91a062003a4e0e28c6e5b39bfb42dfd
Dominik277 <dominikdoric277@gmail.com>
2021-04-03 22:20:48 +0200

    Implementirano da viewmodel observa repozitorij i ispisuje podatke u NaslovnicaFragment.kt.

--- app/src/main/java/Portal/fragmenti/fragmenti/NaslovnicaFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/NaslovnicaFragment.kt
@@ -3,8 +3,10 @@ package Portal.fragmenti.fragmenti
 import Portal.a257.R
 import Portal.a257.databinding.SportFragmentBinding
 import Portal.a257.databinding.ZabavaFragmentBinding
+import Portal.adapter.ObavijestiAdapter
 import Portal.adapter.SportAdapter
 import Portal.adapter.ZabavaAdapter
+import Portal.viewModel.ObavijestiViewModel
 import Portal.viewModel.SportViewModel
 import Portal.viewModel.ZabavaViewModel
 import android.os.Bundle
@@ -13,21 +15,21 @@ import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
+import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.android.synthetic.main.naslovnica_fragment.*
+import kotlinx.android.synthetic.main.obavijesti_fragment.*
 import kotlinx.android.synthetic.main.sport_fragment.*
 import kotlinx.android.synthetic.main.zabava_fragment.*
 
+@AndroidEntryPoint
 class NaslovnicaFragment : Fragment(R.layout.naslovnica_fragment) {
 
     private val sportViewModel: SportViewModel by viewModels()
     private val zabavaViewModel: ZabavaViewModel by viewModels()
-    private lateinit var sportBinding: SportFragmentBinding
-    private lateinit var zabavaBinding: ZabavaFragmentBinding
+    private val obavijestiViewModel: ObavijestiViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        sportBinding = SportFragmentBinding.bind(view)
-        zabavaBinding = ZabavaFragmentBinding.bind(view)
 
         //RecyclerView
         val adapterSport = SportAdapter()
@@ -49,6 +51,17 @@ class NaslovnicaFragment : Fragment(R.layout.naslovnica_fragment) {
         //RasporedViewModel
         zabavaViewModel.readAllDataZabava.observe(viewLifecycleOwner,{ zabava ->
             adapterZabava.setData(zabava)
+        })
+
+        //RecyclerView
+        val adapterObavijesti = ObavijestiAdapter()
+        val recyclerObavijesti = recyclerViewNaslovnica
+        recyclerObavijesti.adapter = adapterObavijesti
+        recyclerObavijesti.layoutManager = LinearLayoutManager(requireContext())
+
+        //RasporedViewModel
+        obavijestiViewModel.readAllDataObavijesti.observe(viewLifecycleOwner,{ obavijesti ->
+            adapterObavijesti.setData(obavijesti)
         })
 
     }
