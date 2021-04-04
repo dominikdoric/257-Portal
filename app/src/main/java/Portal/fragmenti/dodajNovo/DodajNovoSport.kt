445e1777cefb005de1dd1b84edfbefe0c6804a50
Dominik277 <dominikdoric277@gmail.com>
2021-04-04 13:29:47 +0200

    Maknuta naslovnica iz bottom_navigation.xml te ubacen admin.

--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoSport.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoSport.kt
@@ -6,17 +6,12 @@ import Portal.database.table.SportTable
 import Portal.viewModel.SportViewModel
 import android.annotation.SuppressLint
 import android.os.Bundle
-import android.text.TextUtils
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.android.synthetic.main.dodaj_novo_sport_fragment.*
-import kotlinx.android.synthetic.main.dodaj_novo_sport_fragment.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -49,7 +44,6 @@ class DodajNovoSport : Fragment(R.layout.dodaj_novo_sport_fragment) {
         }
 
     }
-
 
     @SuppressLint("SimpleDateFormat")
     private fun insertDataToDatabase() {
