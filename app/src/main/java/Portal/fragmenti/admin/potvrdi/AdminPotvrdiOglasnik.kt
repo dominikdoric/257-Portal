2976e95977129f9f05e6c884e5cbfa02dfddd4fa
Dominik277 <dominikdoric277@gmail.com>
2021-04-06 00:08:02 +0200

    Implementirana logika za prikazivanje.

--- app/src/main/java/Portal/fragmenti/admin/potvrdi/AdminPotvrdiOglasnik.kt
+++ app/src/main/java/Portal/fragmenti/admin/potvrdi/AdminPotvrdiOglasnik.kt
@@ -1,12 +1,36 @@
 package Portal.fragmenti.admin.potvrdi
 
 import Portal.a257.R
+import Portal.a257.databinding.AdminPotvrdiOglasnikBinding
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AdminPotvrdiOglasnik: Fragment(R.layout.admin_potvrdi_oglasnik) {
 
+    private val args by navArgs<AdminPotvrdiOglasnikArgs>()
+    private lateinit var binding: AdminPotvrdiOglasnikBinding
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = AdminPotvrdiOglasnikBinding.bind(view)
+
+        binding.odobriAdminOglasnikNaslov.setText(args.adminOglasnikArgs.oglasnikNaslov)
+        binding.odobriAdminOglasnikClanak.setText(args.adminOglasnikArgs.oglasnikClanak)
+        binding.odobriAdminOglasnikBroj.setText(args.adminOglasnikArgs.oglasnikBroj)
+        binding.odobriAdminOglasnikCijena.setText(args.adminOglasnikArgs.oglasnikCijena)
+        binding.odobriAdminOglasnikLokacija.setText(args.adminOglasnikArgs.oglasnikLokacija)
+
+        binding.gumbAdminOdobriOglasnik.setOnClickListener {
+            odobriOglas()
+        }
+    }
+
+    private fun odobriOglas() {
+        TODO("Not yet implemented")
+    }
 
 }
--- app/src/main/java/Portal/fragmenti/admin/potvrdi/AdminPotvrdiPoljoprivreda.kt
+++ app/src/main/java/Portal/fragmenti/admin/potvrdi/AdminPotvrdiPoljoprivreda.kt
@@ -1,12 +1,33 @@
 package Portal.fragmenti.admin.potvrdi
 
 import Portal.a257.R
+import Portal.a257.databinding.AdminPotvrdiPoljoprivredaBinding
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class AdminPotvrdiPoljoprivreda: Fragment(R.layout.admin_potvrdi_poljoprivreda) {
 
+    private val args by navArgs<AdminPotvrdiPoljoprivredaArgs>()
+    private lateinit var binding: AdminPotvrdiPoljoprivredaBinding
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = AdminPotvrdiPoljoprivredaBinding.bind(view)
+
+        binding.odobriAdminPoljoprivredaNaslov.setText(args.adminPoljoprivredaArgs.poljoprivredaNaslov)
+        binding.odobriAdminPoljoprivredaClanak.setText(args.adminPoljoprivredaArgs.poljoprivredaClanak)
+
+        binding.gumbAdminOdobriPoljoprivreda.setOnClickListener {
+            odobriPoljoprivredu()
+        }
+    }
+
+    private fun odobriPoljoprivredu() {
+        TODO("Not yet implemented")
+    }
 
 }
