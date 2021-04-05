09b2e3edc4319be12ba2f4ca007c214b60038271
Dominik277 <dominikdoric277@gmail.com>
2021-04-05 14:47:49 +0200

    Dodani onClickListeneri na sve gumbove.

--- app/src/main/java/Portal/fragmenti/admin/AdminPrijavljenFragment.kt
+++ app/src/main/java/Portal/fragmenti/admin/AdminPrijavljenFragment.kt
@@ -20,5 +20,36 @@ class AdminPrijavljenFragment: Fragment(R.layout.admin_prijavljen_fragment) {
                AdminPrijavljenFragmentDirections.actionAdminPrijavljenFragmentToAdminOdobriZabavu()
            findNavController().navigate(action)
        }
+
+        binding.gumbAdminObavijest.setOnClickListener {
+            val action = AdminPrijavljenFragmentDirections.actionAdminPrijavljenFragmentToAdminOdobriObavijesti()
+            findNavController().navigate(action)
+        }
+
+        binding.gumbAdminOglasnik.setOnClickListener {
+            val action = AdminPrijavljenFragmentDirections.actionAdminPrijavljenFragmentToAdminOdobriOglasnik()
+            findNavController().navigate(action)
+        }
+
+        binding.gumbAdminPoljoprivreda.setOnClickListener {
+            val action = AdminPrijavljenFragmentDirections.actionAdminPrijavljenFragmentToAdminOdobriPoljoprivreda()
+            findNavController().navigate(action)
+        }
+
+        binding.gumbAdminPrice.setOnClickListener {
+            val action = AdminPrijavljenFragmentDirections.actionAdminPrijavljenFragmentToAdminOdobriPriceCitatelja()
+            findNavController().navigate(action)
+        }
+
+        binding.gumbAdminSport.setOnClickListener {
+            val action = AdminPrijavljenFragmentDirections.actionAdminPrijavljenFragmentToAdminOdobriSport()
+            findNavController().navigate(action)
+        }
+
+        binding.gumbAdminVijest.setOnClickListener {
+            val action = AdminPrijavljenFragmentDirections.actionAdminPrijavljenFragmentToAdminOdobriVijesti()
+            findNavController().navigate(action)
+        }
+
     }
 }
