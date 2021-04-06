6b131666f2aff680cc5ae8e29b75061c306cb1ec
Dominik277 <dominikdoric277@gmail.com>
2021-04-06 17:40:02 +0200

    Kreiran vizualni dio za admin_prijava_sport.xml.

---
+++ app/src/main/java/Portal/fragmenti/adminPrijava/AdminPrijavaSportFragment.kt
@@ -0,0 +1,10 @@
+package Portal.fragmenti.adminPrijava
+
+import Portal.a257.R
+import androidx.fragment.app.Fragment
+
+class AdminPrijavaSportFragment: Fragment(R.layout.admin_prijava_sport) {
+
+
+
+}
