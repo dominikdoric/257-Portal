7cdd77f784b95ebd1337050519f78878ff38c71a
Dominik277 <dominikdoric277@gmail.com>
2021-04-18 20:07:07 +0200

    Implementirana logika za spremanje podataka u firestore.

--- app/src/main/java/Portal/Firestore.kt
+++ app/src/main/java/Portal/Firestore.kt
@@ -2,13 +2,45 @@ package Portal
 
 import Portal.a257.R
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
+import kotlinx.android.synthetic.main.firestore.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
 class Firestore: AppCompatActivity() {
+
+    private val personCollectionRef = Firebase.firestore.collection("persons")
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.firestore)
 
+        btn_firestore.setOnClickListener {
+            val firstName = firestore_firstName.text.toString()
+            val lastName = firestore_lastName.text.toString()
+            val age = firestore_age.text.toString().toInt()
+            val person = Person(firstName, lastName, age)
+            savePerson(person)
+        }
+
     }
+
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionRef.add(person)
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@Firestore,"Successfully saved data",Toast.LENGTH_LONG).show()
+            }
+        }catch (e: Exception){
+            withContext(Dispatchers.Main){
+                Toast.makeText(this@Firestore,e.message,Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
 }
