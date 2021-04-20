41ba2cc8fbfa8b1cde5bc669dac00a040baf174b
Dominik277 <dominikdoric277@gmail.com>
2021-04-20 19:56:17 +0200

    Kreiran vizualni dio za firebase_sport_detail_fragment.xml.

--- app/src/main/java/Portal/firestore/FirestoreSportAdapter.kt
+++ app/src/main/java/Portal/firestore/FirestoreSportAdapter.kt
@@ -4,6 +4,7 @@ import Portal.a257.R
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
@@ -11,16 +12,21 @@ import kotlinx.android.synthetic.main.firestore_jedan_red_sport.view.*
 
 class FirestoreSportAdapter(options: FirestoreRecyclerOptions<SportModel>): FirestoreRecyclerAdapter<SportModel,FirestoreSportAdapter.ViewHolder>(options) {
 
+    private var sportFirestore = emptyList<SportModel>()
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         return  ViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.firestore_jedan_red_sport,parent,false))
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int, model: SportModel) {
+        val currentItem = sportFirestore[position]
+
         holder.sportNaslov.text = model.sportNaslov
         holder.sportVrijeme.text = model.sportVrijeme
 
         holder.itemView.firestoreCardViewSport.setOnClickListener {
-
+            val action = FirestoreSportFragmentDirections.actionFirestoreSportFragmentToFirebaseSportDetailFragment(currentItem)
+            holder.itemView.findNavController().navigate(action)
         }
     }
 
--- app/src/main/java/Portal/firestore/SportModel.kt
+++ app/src/main/java/Portal/firestore/SportModel.kt
@@ -1,8 +1,12 @@
 package Portal.firestore
 
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class SportModel (
     val sportNaslov: String = "",
     val sportClanak: String = "",
     val sportVrijeme: String = "",
     val sportSlika: Int = -1
-        )
+        ): Parcelable
