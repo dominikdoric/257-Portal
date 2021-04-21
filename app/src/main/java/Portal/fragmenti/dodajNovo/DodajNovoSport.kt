0e2dd1d52bd1a7e4844e871a6de9cea464caf7b5
Dominik277 <dominikdoric277@gmail.com>
2021-04-21 12:43:27 +0200

    Implementirana logika za dodavanje podataka u Firebase bazu.

--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoSport.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoSport.kt
@@ -3,6 +3,7 @@ package Portal.fragmenti.dodajNovo
 import Portal.a257.R
 import Portal.a257.databinding.DodajNovoSportFragmentBinding
 import Portal.database.table.SportTable
+import Portal.firestore.SportModel
 import Portal.viewModel.SportViewModel
 import android.annotation.SuppressLint
 import android.os.Bundle
@@ -11,7 +12,14 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.android.synthetic.main.dodaj_novo_sport_fragment.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -21,10 +29,18 @@ class DodajNovoSport : Fragment(R.layout.dodaj_novo_sport_fragment) {
     private val mSportViewModel: SportViewModel by viewModels()
     private lateinit var binding: DodajNovoSportFragmentBinding
 
+    private val personCollectionRef = Firebase.firestore.collection("sport")
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DodajNovoSportFragmentBinding.bind(view)
 
+        gumbSpremiSport.setOnClickListener {
+            val sport = getSport()
+            saveSport(sport)
+        }
+
+        /*
         binding.gumbSpremiSport.setOnClickListener {
             if (binding.etSportNaslov.text.toString().trim().isEmpty()){
                 binding.etSportNaslov.error = "Ovo polje je obavezno!"
@@ -42,7 +58,33 @@ class DodajNovoSport : Fragment(R.layout.dodaj_novo_sport_fragment) {
                 ).show()
             }
         }
+         */
 
+    }
+
+    private fun saveSport(sport: SportModel) = CoroutineScope(Dispatchers.IO).launch{
+        try {
+            personCollectionRef.add(sport)
+            withContext(Dispatchers.Main){
+                Toast.makeText(requireContext(),"Uspješno dodani podaci u bazu",Toast.LENGTH_LONG).show()
+            }
+        }catch (e: Exception){
+            withContext(Dispatchers.Main){
+                Toast.makeText(requireContext(),e.message,Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun getSport(): SportModel{
+        val sdf = SimpleDateFormat("dd.MM.yyyy. HH:mm")
+        val currentDate = sdf.format(Date())
+
+        val naslov = et_sport_naslov.text.toString()
+        val clanak = et_sport_clanak.text.toString()
+        val vrijeme = currentDate
+        val slika = 1
+
+        return SportModel(naslov,clanak,vrijeme,slika)
     }
 
     @SuppressLint("SimpleDateFormat")
