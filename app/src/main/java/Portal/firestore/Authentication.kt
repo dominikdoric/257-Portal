d49368cad6cc427eb7abae86daabe07e0b803d4e
Dominik277 <dominikdoric277@gmail.com>
2021-04-22 10:32:49 +0200

    Implementirana logika za prijavljivanje user-a.

--- app/src/main/java/Portal/firestore/Authentication.kt
+++ app/src/main/java/Portal/firestore/Authentication.kt
@@ -3,10 +3,46 @@ package Portal.firestore
 import Portal.a257.R
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import kotlinx.android.synthetic.main.activity_authentication.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
 
 class Authentication : AppCompatActivity() {
+
+    lateinit var auth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
+        auth = FirebaseAuth.getInstance()
     }
+
+    private fun RegisterUser(){
+        val email = etEmailRegister.text.toString()
+        val password = etPasswordRegister.text.toString()
+        if (email.isNotEmpty() && password.isNotEmpty()){
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    auth.createUserWithEmailAndPassword(email, password).await()
+                    withContext(Dispatchers.Main){
+                        checkLoggedInState()
+                    }
+                }catch (e: Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@Authentication,e.message,Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun checkLoggedInState() {
+
+    }
+
 }
