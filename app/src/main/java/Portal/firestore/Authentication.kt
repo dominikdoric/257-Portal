62851db098d317619f70f940865f03c0993a3526
Dominik277 <dominikdoric277@gmail.com>
2021-04-22 09:57:56 +0200

    Dodan dependecy za Firebase authentication i dodana nova aktivnost.

---
+++ app/src/main/java/Portal/firestore/Authentication.kt
@@ -0,0 +1,12 @@
+package Portal.firestore
+
+import Portal.a257.R
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Authentication : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_authentication)
+    }
+}
