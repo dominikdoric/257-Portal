be6f97c9c9e85058bdb99dc7049e220e108dd01d
Dominik277 <dominikdoric277@gmail.com>
2021-04-22 11:23:56 +0200

    Implementirana logika za updejtanje profila.

--- app/src/main/java/Portal/firestore/Authentication.kt
+++ app/src/main/java/Portal/firestore/Authentication.kt
@@ -1,16 +1,19 @@
 package Portal.firestore
 
 import Portal.a257.R
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.UserProfileChangeRequest
 import kotlinx.android.synthetic.main.activity_authentication.*
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
+import java.net.URI
 
 class Authentication : AppCompatActivity() {
 
@@ -20,7 +23,6 @@ class Authentication : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_authentication)
         auth = FirebaseAuth.getInstance()
-        auth.signOut()
 
         btnRegister.setOnClickListener {
             registerUser()
@@ -30,11 +32,38 @@ class Authentication : AppCompatActivity() {
             loginUser()
         }
 
+        btnUpdateProfile.setOnClickListener {
+            updateProfil()
+        }
+
     }
 
     override fun onStart() {
         super.onStart()
         checkLoggedInState()
+    }
+
+    private fun updateProfil(){
+        auth.currentUser?.let { user ->
+            val userName = etUsername.text.toString()
+            val photoURI = Uri.parse("android.resource:///$packageName/${R.drawable.jaksic}")
+            val profileUpdate = UserProfileChangeRequest.Builder()
+                .setDisplayName(userName)
+                .setPhotoUri(photoURI)
+                .build()
+            CoroutineScope(Dispatchers.IO).launch {
+                try {
+                    user.updateProfile(profileUpdate).await()
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@Authentication,"Successfully updated",Toast.LENGTH_LONG).show()
+                    }
+                }catch (e: Exception){
+                    withContext(Dispatchers.Main){
+                        Toast.makeText(this@Authentication,e.message,Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
     }
 
     private fun registerUser(){
