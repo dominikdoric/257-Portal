e4046e297226ebe13128c859da9cdfcfce654bbf
Dominik277 <dominikdoric277@gmail.com>
2021-04-24 23:40:53 +0200

    Izbrisane sve navigacije u navigacijskom grafu za admina.

--- app/src/main/java/Portal/adapter/adminAdapter/AdminObavijestiAdapter.kt
+++ app/src/main/java/Portal/adapter/adminAdapter/AdminObavijestiAdapter.kt
@@ -9,7 +9,6 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
-import kotlinx.android.synthetic.main.jedan_red_obavijesti.view.*
 
 class AdminObavijestiAdapter() : RecyclerView.Adapter<AdminObavijestiAdapter.ViewHolder>() {
 
