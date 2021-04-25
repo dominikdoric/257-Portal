238ef449c728f2abf1b0aef129d8c4d40629027b
Dominik277 <dominikdoric277@gmail.com>
2021-04-25 23:11:07 +0200

    Implementiran SportAdapter.kt.

--- app/src/main/java/Portal/adapter/SportAdapter.kt
+++ app/src/main/java/Portal/adapter/SportAdapter.kt
@@ -1,24 +1,55 @@
 package Portal.adapter
 
+import Portal.a257.databinding.JedanRedSportBinding
+import Portal.a257.databinding.JedanRedVijestiBinding
+import Portal.database.table.SportTable
+import Portal.fragmenti.fragmenti.VijestiFragmentDirections
+import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 
 class SportAdapter: RecyclerView.Adapter<SportAdapter.ViewHolder>()  {
 
+    private var sportList = emptyList<SportTable>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SportAdapter.ViewHolder {
-        TODO("Not yet implemented")
+        return ViewHolder(
+            JedanRedSportBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
     }
 
     override fun onBindViewHolder(holder: SportAdapter.ViewHolder, position: Int) {
-        TODO("Not yet implemented")
+        val currentItem = sportList[position]
+        holder.binding.textViewSportNaslov.text = currentItem.sportNaslov
+        holder.binding.textViewSportVrijeme.text = currentItem.sportVrijeme
+
+        holder.binding.cardViewSport.setOnLongClickListener {
+            val action =
+                VijestiFragmentDirections.actionVijestiNavDrawerToAdminPrijavaVijestiFragment(
+                    currentItem
+                )
+            holder.itemView.findNavController().navigate(action)
+            true
+        }
+
+        holder.binding.cardViewSport.setOnClickListener {
+            val action =
+                VijestiFragmentDirections.actionVijestiNavDrawerToDetailVijestiFragment(currentItem)
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return sportList.size
     }
 
-    class ViewHolder {
+    inner class ViewHolder(val binding: JedanRedSportBinding): RecyclerView.ViewHolder(binding.root) {
 
     }
 
