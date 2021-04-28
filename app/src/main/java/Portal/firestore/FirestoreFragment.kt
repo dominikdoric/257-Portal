c9ea5181c89d82755ac126d7931da150ccc2e6af
Dominik277 <dominikdoric277@gmail.com>
2021-04-28 11:46:49 +0200

    Implmenetirana logika za spremanje podataka u Firebase Firestore.

--- app/src/main/java/Portal/firestore/FirestoreFragment.kt
+++ app/src/main/java/Portal/firestore/FirestoreFragment.kt
@@ -1,6 +1,7 @@
 package Portal.firestore
 
 import Portal.a257.R
+import Portal.a257.databinding.FirestoreFragmentBinding
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
@@ -10,22 +11,34 @@ import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import java.lang.Exception
 
 class FirestoreFragment: Fragment(R.layout.firestore_fragment) {
 
     private val personCollectionRef = Firebase.firestore.collection("persons")
+    private lateinit var binding: FirestoreFragmentBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FirestoreFragmentBinding.bind(view)
 
-
+        binding.firstoreSpremi.setOnClickListener {
+            val firstName = binding.firstName.text.toString()
+            val lastName = binding.lastName.text.toString()
+            val age = binding.age.text.toString().toInt()
+            val person = Person(firstName, lastName, age)
+            savePerson(person)
+        }
     }
 
     private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch{
         try {
-
+            personCollectionRef.add(person).await()
+            withContext(Dispatchers.Main){
+                Toast.makeText(requireContext(),"Uspješno spremljeno!",Toast.LENGTH_LONG).show()
+            }
         }catch (e: Exception){
             withContext(Dispatchers.Main){
                 Toast.makeText(requireContext(),e.message,Toast.LENGTH_LONG).show()
