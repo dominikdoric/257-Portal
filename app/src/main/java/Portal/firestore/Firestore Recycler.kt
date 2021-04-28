455f63f6ae9849a43ec7c7fa956240af81f793c7
Dominik277 <dominikdoric277@gmail.com>
2021-04-28 23:09:37 +0200

    Dodan FirestoreRecycler.kt u navigacijski graf i implementirano da se otvara taj fragment na klik menu item-a.

--- app/src/main/java/Portal/firestore/Firestore Recycler.kt
+++
@@ -1,4 +0,0 @@
-package Portal.firestore
-
-class `Firestore Recycler` {
-}
---
+++ app/src/main/java/Portal/firestore/FirestoreRecycler.kt
@@ -0,0 +1,14 @@
+package Portal.firestore
+
+import Portal.a257.R
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class FirestoreRecycler: Fragment(R.layout.firestore_recycler) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
