8c453511ae80c1a1735864d7c7c6fe093991c571
Dominik277 <dominikdoric277@gmail.com>
2021-04-28 12:11:30 +0200

    Implementirana logika za prikazivanje podataka iz Firestore-a unutar TextView-a.

--- app/src/main/java/Portal/firestore/FirestoreFragment.kt
+++ app/src/main/java/Portal/firestore/FirestoreFragment.kt
@@ -7,6 +7,7 @@ import android.view.View
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -14,8 +15,9 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 import java.lang.Exception
+import java.lang.StringBuilder
 
-class FirestoreFragment: Fragment(R.layout.firestore_fragment) {
+class FirestoreFragment : Fragment(R.layout.firestore_fragment) {
 
     private val personCollectionRef = Firebase.firestore.collection("persons")
     private lateinit var binding: FirestoreFragmentBinding
@@ -31,17 +33,40 @@ class FirestoreFragment: Fragment(R.layout.firestore_fragment) {
             val person = Person(firstName, lastName, age)
             savePerson(person)
         }
+
+        binding.firstorePrikaži.setOnClickListener {
+            retrievePersons()
+        }
+
     }
 
-    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch{
+    private fun savePerson(person: Person) = CoroutineScope(Dispatchers.IO).launch {
         try {
             personCollectionRef.add(person).await()
-            withContext(Dispatchers.Main){
-                Toast.makeText(requireContext(),"Uspješno spremljeno!",Toast.LENGTH_LONG).show()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), "Uspješno spremljeno!", Toast.LENGTH_LONG).show()
             }
-        }catch (e: Exception){
-            withContext(Dispatchers.Main){
-                Toast.makeText(requireContext(),e.message,Toast.LENGTH_LONG).show()
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    private fun retrievePersons() = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val querySnapshot = personCollectionRef.get().await()
+            val sb = StringBuilder()
+            for (document in querySnapshot.documents) {
+                val person = document.toObject<Person>()
+                sb.append("$person\n")
+            }
+            withContext(Dispatchers.Main) {
+                binding.textViewFirestore.text = sb.toString()
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
             }
         }
     }
