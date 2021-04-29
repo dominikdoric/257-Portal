2c0ccf99aae06d05c5ad65c5a526edb4d1b356d0
Dominik277 <dominikdoric277@gmail.com>
2021-04-29 08:55:55 +0200

    Dodana akcija u navigacijski graf i animacije.

--- app/src/main/java/Portal/firestore/FirestoreDetail.kt
+++ app/src/main/java/Portal/firestore/FirestoreDetail.kt
@@ -1,14 +1,18 @@
 package Portal.firestore
 
 import Portal.a257.R
+import Portal.a257.databinding.FirestoreDetailBinding
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 
 class FirestoreDetail : Fragment(R.layout.firestore_detail) {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
     }
 
 }
