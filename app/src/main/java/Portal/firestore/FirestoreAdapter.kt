813a8590ddf4439eb3cd7e6c44193c963161a41f
Dominik277 <dominikdoric277@gmail.com>
2021-04-29 09:09:55 +0200

    Dodano par stvari.

--- app/src/main/java/Portal/firestore/FirestoreAdapter.kt
+++ app/src/main/java/Portal/firestore/FirestoreAdapter.kt
@@ -1,16 +1,17 @@
 package Portal.firestore
 
-import Portal.a257.R
 import Portal.a257.databinding.FirestoreJedanRedBinding
 import android.view.LayoutInflater
-import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
 class FirestoreAdapter(options: FirestoreRecyclerOptions<Person>) :
     FirestoreRecyclerAdapter<Person, FirestoreAdapter.PersonViewHolder>(options) {
+
+    private var personList = emptyList<Person>()
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PersonViewHolder {
         return PersonViewHolder(
@@ -23,9 +24,16 @@ class FirestoreAdapter(options: FirestoreRecyclerOptions<Person>) :
     }
 
     override fun onBindViewHolder(holder: PersonViewHolder, position: Int, model: Person) {
+        val currentItem = model[position]
+
         holder.firstName.text = model.firstName
         holder.lastName.text = model.lastName
         holder.age.text = model.age.toString()
+
+        holder.binding.jedanRedFirestore.setOnClickListener {
+            val action = FirestoreRecyclerDirections.actionFirestoreRecyclerToFirestoreDetail()
+            holder.itemView.findNavController().navigate(action)
+        }
     }
 
     class PersonViewHolder(val binding: FirestoreJedanRedBinding) :
--- app/src/main/java/Portal/firestore/FirestoreDetail.kt
+++ app/src/main/java/Portal/firestore/FirestoreDetail.kt
@@ -9,10 +9,16 @@ import androidx.navigation.fragment.navArgs
 
 class FirestoreDetail : Fragment(R.layout.firestore_detail) {
 
+    private val args by navArgs<FirestoreDetailArgs>()
+    private lateinit var binding: FirestoreDetailBinding
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = FirestoreDetailBinding.bind(view)
 
+        binding.firestoreDetailFirstName.text = args.firestoreArgs.firstName
+        binding.firestoreDetailLastName.text = args.firestoreArgs.lastName
+        binding.firestoreDetailAge.text = args.firestoreArgs.age.toString()
 
     }
-
 }
