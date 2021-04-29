44274f19323b7008346844cad8cb5ecfa7540380
Dominik277 <dominikdoric277@gmail.com>
2021-04-29 08:42:18 +0200

    Dodan novi fragment za detail te njegov pripadajuci layout file.

---
+++ app/src/main/java/Portal/firestore/FirestoreDetail.kt
@@ -0,0 +1,14 @@
+package Portal.firestore
+
+import Portal.a257.R
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class FirestoreDetail : Fragment(R.layout.firestore_detail) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
--- app/src/main/java/Portal/firestore/Person.kt
+++ app/src/main/java/Portal/firestore/Person.kt
@@ -1,7 +1,11 @@
 package Portal.firestore
 
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+@Parcelize
 data class Person (
     var firstName: String = "",
     var lastName: String = "",
     var age: Int = -1
-        )
+        ): Parcelable
