e7498a3203d41368db84493a68c3c9cb6e637b56
Dominik277 <dominikdoric277@gmail.com>
2021-04-29 09:47:48 +0200

    Promjenjen RecyclerView adapter.

--- app/src/main/java/Portal/firestore/FirestoreAdapter.kt
+++ app/src/main/java/Portal/firestore/FirestoreAdapter.kt
@@ -8,8 +8,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.firebase.ui.firestore.FirestoreRecyclerAdapter
 import com.firebase.ui.firestore.FirestoreRecyclerOptions
 
-class FirestoreAdapter(options: FirestoreRecyclerOptions<Person>) :
-    FirestoreRecyclerAdapter<Person, FirestoreAdapter.PersonViewHolder>(options) {
+class FirestoreAdapter : RecyclerView.Adapter<FirestoreAdapter.PersonViewHolder>() {
 
     private var personList = emptyList<Person>()
 
@@ -23,17 +22,22 @@ class FirestoreAdapter(options: FirestoreRecyclerOptions<Person>) :
         )
     }
 
-    override fun onBindViewHolder(holder: PersonViewHolder, position: Int, model: Person) {
-        val currentItem = model[position]
+    override fun onBindViewHolder(holder: PersonViewHolder, position: Int) {
+        val currentItem = personList[position]
 
-        holder.firstName.text = model.firstName
-        holder.lastName.text = model.lastName
-        holder.age.text = model.age.toString()
+        holder.firstName.text = currentItem.firstName
+        holder.lastName.text = currentItem.lastName
+        holder.age.text = currentItem.age.toString()
 
         holder.binding.jedanRedFirestore.setOnClickListener {
-            val action = FirestoreRecyclerDirections.actionFirestoreRecyclerToFirestoreDetail()
+            val action =
+                FirestoreRecyclerDirections.actionFirestoreRecyclerToFirestoreDetail(currentItem)
             holder.itemView.findNavController().navigate(action)
         }
+    }
+
+    override fun getItemCount(): Int {
+        return personList.size
     }
 
     class PersonViewHolder(val binding: FirestoreJedanRedBinding) :
