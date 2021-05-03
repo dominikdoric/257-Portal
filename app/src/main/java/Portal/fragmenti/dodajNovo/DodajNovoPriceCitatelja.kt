ac73f84cbed14b407eedf986ca3d82793e396bb7
Dominik277 <dominikdoric277@gmail.com>
2021-05-03 19:07:50 +0200

    Implementirano da se prikazuju podaci u PriceCitateljaFragment.kt.

--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoPriceCitatelja.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoPriceCitatelja.kt
@@ -3,23 +3,27 @@ package Portal.fragmenti.dodajNovo
 import Portal.a257.R
 import Portal.a257.databinding.DodajNovoPriceCitateljaFragmentBinding
 import Portal.database.table.PriceCitateljaTable
-import Portal.viewModel.PriceCitateljaViewModel
+import Portal.database.table.SportTable
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
 import android.widget.Toast
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.navigation.fragment.findNavController
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 import dagger.hilt.android.AndroidEntryPoint
-import java.text.SimpleDateFormat
-import java.util.*
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.tasks.await
+import kotlinx.coroutines.withContext
+import java.lang.Exception
+import java.lang.StringBuilder
 
 @AndroidEntryPoint
 class DodajNovoPriceCitatelja : Fragment(R.layout.dodaj_novo_price_citatelja_fragment) {
 
-    private val mPriceCitateljaViewModel: PriceCitateljaViewModel by viewModels()
+    private val personCollectionRef = Firebase.firestore.collection("priceCitatelja")
     private lateinit var binding: DodajNovoPriceCitateljaFragmentBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -27,24 +31,47 @@ class DodajNovoPriceCitatelja : Fragment(R.layout.dodaj_novo_price_citatelja_fra
         binding = DodajNovoPriceCitateljaFragmentBinding.bind(view)
 
         binding.gumbSpremiPriceCitatelja.setOnClickListener {
-            if (binding.etPriceCitateljaNaslov.text.toString().trim().isEmpty()){
-                binding.etPriceCitateljaNaslov.error = "Ovo polje je obavezno!"
-            }else if (binding.etPriceCitateljaClanak.text.toString().trim().isEmpty()){
-                binding.etPriceCitateljaClanak.error = "Ovo polje je obavezno"
+            val naslov = binding.naslov.text.toString()
+            val clanak = binding.clanak.text.toString()
+            val vrijeme = binding.vrijeme.text.toString()
+            val priceCitatelja = PriceCitateljaTable(naslov, clanak, vrijeme)
+            savePerson(priceCitatelja)
+        }
+
+        binding.gumbPrikaziPriceCitatelja.setOnClickListener {
+            retrievePersons()
+        }
+    }
+
+    private fun savePerson(priceCitatelja: PriceCitateljaTable) = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            personCollectionRef.add(priceCitatelja).await()
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), "Uspješno spremljeno!", Toast.LENGTH_LONG).show()
             }
-            else{
-                insertDataToDatabase()
-                val action = DodajNovoPriceCitateljaDirections.actionMenuDodajNovuPricuCitateljaToPriceCitateljaNavDrawer()
-                findNavController().navigate(action)
-                Toast.makeText(
-                    requireContext(),
-                    "Vaš članak je zaprimljen te je poslan adminu na odobrenje.Hvala!",
-                    Toast.LENGTH_LONG
-                ).show()
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
             }
         }
     }
 
+    private fun retrievePersons() = CoroutineScope(Dispatchers.IO).launch {
+        try {
+            val querySnapshot = personCollectionRef.get().await()
+            val sb = StringBuilder()
+            for (document in querySnapshot.documents) {
+                val priceCitatelja = document.toObject<PriceCitateljaTable>()
+                sb.append("$priceCitatelja\n")
+            }
+        } catch (e: Exception) {
+            withContext(Dispatchers.Main) {
+                Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
+    /*
     private fun insertDataToDatabase() {
         val sdf = SimpleDateFormat("dd.MM.yyyy. HH:mm")
         val currentDate = sdf.format(Date())
@@ -57,5 +84,6 @@ class DodajNovoPriceCitatelja : Fragment(R.layout.dodaj_novo_price_citatelja_fra
         val priceCitatelja = PriceCitateljaTable(0, noviNaslov, noviClanak, novoVrijeme, novaSlika)
         mPriceCitateljaViewModel.addPriceCitatelja(priceCitatelja)
     }
+     */
 
 }
--- app/src/main/java/Portal/fragmenti/fragmenti/PriceCitateljaFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/PriceCitateljaFragment.kt
@@ -3,20 +3,26 @@ package Portal.fragmenti.fragmenti
 import Portal.a257.R
 import Portal.a257.databinding.PriceCitateljaFragmentBinding
 import Portal.adapter.PriceCitateljaAdapter
-import Portal.viewModel.PriceCitateljaViewModel
+import Portal.adapter.SportAdapter
+import Portal.database.table.PriceCitateljaTable
+import Portal.database.table.SportTable
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.google.firebase.firestore.CollectionReference
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.firestore.Query
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class PriceCitateljaFragment : Fragment(R.layout.price_citatelja_fragment) {
 
-    private val mPriceCitateljaViewModel: PriceCitateljaViewModel by viewModels()
+    private val db: FirebaseFirestore = FirebaseFirestore.getInstance()
+    private val collectionReference: CollectionReference = db.collection("priceCitatelja")
+    var priceCitateljaAdapter: PriceCitateljaAdapter? = null
     private lateinit var binding: PriceCitateljaFragmentBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,20 +35,29 @@ class PriceCitateljaFragment : Fragment(R.layout.price_citatelja_fragment) {
                 DividerItemDecoration.VERTICAL
             )
         )
+        setUpRecyclerView()
+    }
 
-        //RecyclerView
-        val adapter = PriceCitateljaAdapter()
-        val recyclerPriceCitatelja = binding.recyclerViewPriceCitatelja
-        recyclerPriceCitatelja.adapter = adapter
-        recyclerPriceCitatelja.layoutManager = LinearLayoutManager(requireContext())
+    private fun setUpRecyclerView() {
+        val query: Query = collectionReference
+        val tableRecyclerOptions: FirestoreRecyclerOptions<PriceCitateljaTable> =
+            FirestoreRecyclerOptions.Builder<PriceCitateljaTable>()
+                .setQuery(query, PriceCitateljaTable::class.java)
+                .build()
+        priceCitateljaAdapter = PriceCitateljaAdapter(tableRecyclerOptions)
 
-        //ViewModel
-        mPriceCitateljaViewModel.readAllDataPriceCitatelja.observe(
-            viewLifecycleOwner,
-            Observer { priceCitatelja ->
-                adapter.setData(priceCitatelja)
-            })
+        binding.recyclerViewPriceCitatelja.layoutManager = LinearLayoutManager(requireContext())
+        binding.recyclerViewPriceCitatelja.adapter = priceCitateljaAdapter
+    }
 
+    override fun onStart() {
+        super.onStart()
+        priceCitateljaAdapter!!.startListening()
+    }
+
+    override fun onStop() {
+        super.onStop()
+        priceCitateljaAdapter!!.stopListening()
     }
 
 }
