842ff33621b08cdd156e05a49141f79359e33ca1
Dominik277 <dominikdoric277@gmail.com>
2021-05-03 15:27:08 +0200

    Izbrisano dosta toga.

--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoObavijesti.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoObavijesti.kt
@@ -3,7 +3,6 @@ package Portal.fragmenti.dodajNovo
 import Portal.a257.R
 import Portal.a257.databinding.DodajNovoObavijestiFragmentBinding
 import Portal.database.table.ObavijestiTable
-import Portal.viewModel.ObavijestiViewModel
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.view.View
@@ -18,32 +17,16 @@ import java.util.*
 @AndroidEntryPoint
 class DodajNovoObavijesti : Fragment(R.layout.dodaj_novo_obavijesti_fragment) {
 
-    private val mObavijestViewModel: ObavijestiViewModel by viewModels()
     private lateinit var binding: DodajNovoObavijestiFragmentBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DodajNovoObavijestiFragmentBinding.bind(view)
 
-        binding.gumbSpremiObavijest.setOnClickListener {
-            if (binding.etObavijestiNaslov.text.toString().trim().isEmpty()){
-                binding.etObavijestiNaslov.error = "Ovo polje je obavezno!"
-            }else if (binding.etObavijestiClanak.text.toString().trim().isEmpty()){
-                binding.etObavijestiClanak.error = "Ovo polje je obavezno"
-            }
-            else{
-                insertDataToDatabase()
-                val action = DodajNovoObavijestiDirections.actionMenuDodajNovuObavijestToObavijestiNavDrawer()
-                findNavController().navigate(action)
-                Toast.makeText(
-                    requireContext(),
-                    "Vaš članak je zaprimljen te je poslan adminu na odobrenje.Hvala!",
-                    Toast.LENGTH_LONG
-                ).show()
-            }
-        }
+
     }
 
+    /*
     @SuppressLint("SimpleDateFormat")
     private fun insertDataToDatabase() {
         val sdf = SimpleDateFormat("dd.MM.yyyy. HH:mm")
@@ -57,4 +40,5 @@ class DodajNovoObavijesti : Fragment(R.layout.dodaj_novo_obavijesti_fragment) {
         val obavijesti = ObavijestiTable(0, noviNaslov, noviClanak, novoVrijeme, novaSlika)
         mObavijestViewModel.addObavijesti(obavijesti)
     }
+     */
 }
--- app/src/main/java/Portal/fragmenti/fragmenti/ObavijestiFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/ObavijestiFragment.kt
@@ -2,21 +2,15 @@ package Portal.fragmenti.fragmenti
 
 import Portal.a257.R
 import Portal.a257.databinding.ObavijestiFragmentBinding
-import Portal.adapter.ObavijestiAdapter
-import Portal.viewModel.ObavijestiViewModel
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.DividerItemDecoration
-import androidx.recyclerview.widget.LinearLayoutManager
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class ObavijestiFragment : Fragment(R.layout.obavijesti_fragment) {
 
-    private val mObavijestiViewModel: ObavijestiViewModel by viewModels()
     private lateinit var binding: ObavijestiFragmentBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -29,17 +23,6 @@ class ObavijestiFragment : Fragment(R.layout.obavijesti_fragment) {
                 (binding.recyclerViewObavijesti.context, DividerItemDecoration.VERTICAL)
         )
 
-        //RecyclerView
-        val adapter = ObavijestiAdapter()
-        val recyclerObavijesti = binding.recyclerViewObavijesti
-        recyclerObavijesti.adapter = adapter
-        recyclerObavijesti.layoutManager = LinearLayoutManager(requireContext())
 
-        //RasporedViewModel
-        mObavijestiViewModel.readAllDataObavijesti.observe(
-            viewLifecycleOwner,
-            Observer { obavijesti ->
-                adapter.setData(obavijesti)
-            })
     }
 }
