a900ebcf2d7f8db78beea5a0bd1c071ba430e0af
Dominik277 <dominikdoric277@gmail.com>
2021-05-05 23:22:32 +0200

    Kreiran vizualni dio za pokus2_fragment.xml.

--- app/src/main/java/Portal/fragmenti/fragmenti/Pokus.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/Pokus.kt
@@ -1,10 +1,27 @@
 package Portal.fragmenti.fragmenti
 
 import Portal.a257.R
+import Portal.a257.databinding.PokusFragmentBinding
+import Portal.model.PokusTable
+import android.os.Bundle
+import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 
 class Pokus: Fragment(R.layout.pokus_fragment) {
 
+    private lateinit var binding: PokusFragmentBinding
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding = PokusFragmentBinding.bind(view)
+
+        val pokus = PokusTable("Dominik","Doric")
+
+        binding.gumbSafeArgs.setOnClickListener {
+            val action = PokusDirections.actionMenuPokusToPokus2(pokus)
+            findNavController().navigate(action)
+        }
+    }
 
 }
