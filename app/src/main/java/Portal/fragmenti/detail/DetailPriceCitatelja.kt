44379ee2f65c10866ad50e4c576c2da673c710a5
Dominik277 <dominikdoric277@gmail.com>
2021-05-06 10:58:50 +0200

    Kreiran novi detail fragment te dodan u navigacijski graf.

---
+++ app/src/main/java/Portal/fragmenti/detail/DetailPriceCitatelja.kt
@@ -0,0 +1,14 @@
+package Portal.fragmenti.detail
+
+import Portal.a257.R
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class DetailPriceCitatelja: Fragment(R.layout.detail_price_citatelja) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+}
