b3f94009ad80200852daf76a0014b7f4122d1b60
Dominik277 <dominikdoric277@gmail.com>
2021-05-06 18:41:31 +0200

    Dodan novi fragment u navigacijski graf.

---
+++ app/src/main/java/Portal/fragmenti/detail/DetailZabava.kt
@@ -0,0 +1,17 @@
+package Portal.fragmenti.detail
+
+import Portal.a257.R
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class DetailZabava: Fragment(R.layout.detail_zabava) {
+
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
