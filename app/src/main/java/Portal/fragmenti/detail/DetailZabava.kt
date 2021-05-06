995c6179ca3e153495af7b80dcea060fbedc4ec6
Dominik277 <dominikdoric277@gmail.com>
2021-05-06 18:42:43 +0200

    Kreiran vizualni dio za detail_zabava.xml.

--- app/src/main/java/Portal/fragmenti/detail/DetailZabava.kt
+++ app/src/main/java/Portal/fragmenti/detail/DetailZabava.kt
@@ -1,16 +1,20 @@
 package Portal.fragmenti.detail
 
 import Portal.a257.R
+import Portal.a257.databinding.DetailZabavaBinding
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 
 class DetailZabava: Fragment(R.layout.detail_zabava) {
 
-
+    private lateinit var binding: DetailZabavaBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding = DetailZabavaBinding.bind(view)
+
+
 
     }
 
