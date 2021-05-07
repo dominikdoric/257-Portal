1148444579bbe607325ea4fdac8c1dce3d8f194b
Dominik277 <dominikdoric277@gmail.com>
2021-05-07 09:49:50 +0200

    Dodan novi fragment te dodan i u navigacijski graf.

---
+++ app/src/main/java/Portal/fragmenti/updateDelete/UpdateDeletePoljoprivreda.kt
@@ -0,0 +1,13 @@
+package Portal.fragmenti.updateDelete
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class UpdateDeletePoljoprivreda: Fragment() {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+
+}
