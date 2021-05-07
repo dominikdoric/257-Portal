ced46061c3640e41232a5ed3293ce9ff70e8f55f
Dominik277 <dominikdoric277@gmail.com>
2021-05-07 10:05:37 +0200

    Implementiran do kraja UpdateDeletePriceCitatelja.kt i prijenos podataka u taj fragment iz recyclerview-a.

--- app/src/main/java/Portal/adapter/PriceCitateljaAdapter.kt
+++ app/src/main/java/Portal/adapter/PriceCitateljaAdapter.kt
@@ -3,6 +3,7 @@ package Portal.adapter
 import Portal.a257.databinding.JedanRedPriceCitateljaBinding
 import Portal.fragmenti.fragmenti.PriceCitateljaFragmentDirections
 import Portal.model.PriceCitateljaTable
+import android.annotation.SuppressLint
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -14,7 +15,9 @@ import java.text.SimpleDateFormat
 import java.util.*
 
 class PriceCitateljaAdapter(options: FirestoreRecyclerOptions<PriceCitateljaTable>) :
-    FirestoreRecyclerAdapter<PriceCitateljaTable, PriceCitateljaAdapter.PriceCitateljaViewHolder>(options) {
+    FirestoreRecyclerAdapter<PriceCitateljaTable, PriceCitateljaAdapter.PriceCitateljaViewHolder>(
+        options
+    ) {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PriceCitateljaViewHolder {
         return PriceCitateljaViewHolder(
@@ -26,7 +29,12 @@ class PriceCitateljaAdapter(options: FirestoreRecyclerOptions<PriceCitateljaTabl
         )
     }
 
-    override fun onBindViewHolder(holder: PriceCitateljaViewHolder, position: Int, model: PriceCitateljaTable) {
+    @SuppressLint("SimpleDateFormat")
+    override fun onBindViewHolder(
+        holder: PriceCitateljaViewHolder,
+        position: Int,
+        model: PriceCitateljaTable
+    ) {
         val sdf = SimpleDateFormat("dd.MM.yyyy. HH:mm")
         val currentDate = sdf.format(Date())
 
@@ -34,13 +42,21 @@ class PriceCitateljaAdapter(options: FirestoreRecyclerOptions<PriceCitateljaTabl
         holder.naslov.text = model.priceCitateljaNaslov
 
         holder.binding.cardViewPricaCitatelja.setOnClickListener { v: View ->
-            val data = PriceCitateljaTable(model.priceCitateljaNaslov,model.priceCitateljaClanak)
+            val data = PriceCitateljaTable(model.priceCitateljaNaslov, model.priceCitateljaClanak)
             val action = PriceCitateljaFragmentDirections.actionPriceCitateljaNavDrawerToDetailPriceCitatelja(data)
             v.findNavController().navigate(action)
         }
+
+        holder.binding.cardViewPricaCitatelja.setOnLongClickListener { v: View ->
+            val data = PriceCitateljaTable(model.priceCitateljaNaslov, model.priceCitateljaClanak)
+            val action = PriceCitateljaFragmentDirections.actionPriceCitateljaNavDrawerToUpdateDeletePriceCitatelja(data)
+            v.findNavController().navigate(action)
+            true
+        }
     }
 
-    class PriceCitateljaViewHolder(val binding: JedanRedPriceCitateljaBinding) : RecyclerView.ViewHolder(binding.root) {
+    class PriceCitateljaViewHolder(val binding: JedanRedPriceCitateljaBinding) :
+        RecyclerView.ViewHolder(binding.root) {
         var naslov = binding.naslov
         var vrijeme = binding.vrijeme
     }
--- app/src/main/java/Portal/fragmenti/updateDelete/UpdateDeletePriceCitatelja.kt
+++ app/src/main/java/Portal/fragmenti/updateDelete/UpdateDeletePriceCitatelja.kt
@@ -5,16 +5,35 @@ import Portal.a257.databinding.UpdateDeletePriceCitateljaBinding
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.navArgs
 
 class UpdateDeletePriceCitatelja: Fragment(R.layout.update_delete_price_citatelja) {
 
     private lateinit var binding: UpdateDeletePriceCitateljaBinding
-
+    private val args by navArgs<UpdateDeletePriceCitateljaArgs>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = UpdateDeletePriceCitateljaBinding.bind(view)
 
+        binding.naslov.setText(args.updateCitateljeArgs.priceCitateljaNaslov)
+        binding.clanak.setText(args.updateCitateljeArgs.priceCitateljaClanak)
+
+        binding.gumbAzuriraj.setOnClickListener {
+            azurirajItem()
+        }
+
+        binding.gumbObrisi.setOnClickListener {
+            obrisiItem()
+        }
+
     }
 
+    private fun obrisiItem() {
+
+    }
+
+    private fun azurirajItem() {
+
+    }
 }
