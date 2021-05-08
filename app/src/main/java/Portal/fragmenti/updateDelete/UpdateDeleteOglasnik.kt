f0f0503a33e614c95cccf602acc24766a0cb7c51
Dominik277 <dominikdoric277@gmail.com>
2021-05-08 13:43:18 +0200

    Implementiran UpdateDeleteOglasnik.kt do kraja.

--- app/src/main/java/Portal/fragmenti/updateDelete/UpdateDeleteOglasnik.kt
+++ app/src/main/java/Portal/fragmenti/updateDelete/UpdateDeleteOglasnik.kt
@@ -9,6 +9,7 @@ import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import androidx.navigation.fragment.navArgs
+import com.google.firebase.firestore.SetOptions
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import kotlinx.coroutines.CoroutineScope
@@ -17,7 +18,7 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 import kotlinx.coroutines.withContext
 
-class UpdateDeleteOglasnik: Fragment(R.layout.update_delete_oglasnik) {
+class UpdateDeleteOglasnik : Fragment(R.layout.update_delete_oglasnik) {
 
     private lateinit var binding: UpdateDeleteOglasnikBinding
     private val args by navArgs<UpdateDeleteOglasnikArgs>()
@@ -27,32 +28,62 @@ class UpdateDeleteOglasnik: Fragment(R.layout.update_delete_oglasnik) {
         super.onViewCreated(view, savedInstanceState)
         binding = UpdateDeleteOglasnikBinding.bind(view)
 
-        binding.naslov.setText(args.updateOglasnikArgs.oglasnikNaslov)
-        binding.clanak.setText(args.updateOglasnikArgs.oglasnikClanak)
-        binding.cijena.setText(args.updateOglasnikArgs.oglasnikCijena)
-        binding.lokacija.setText(args.updateOglasnikArgs.oglasnikLokacija)
-        binding.broj.setText(args.updateOglasnikArgs.oglasnikBroj)
+        binding.naslovStari.setText(args.updateOglasnikArgs.oglasnikNaslov)
+        binding.clanakStari.setText(args.updateOglasnikArgs.oglasnikClanak)
+        binding.cijenaStari.setText(args.updateOglasnikArgs.oglasnikCijena)
+        binding.lokacijaStari.setText(args.updateOglasnikArgs.oglasnikLokacija)
+        binding.brojStari.setText(args.updateOglasnikArgs.oglasnikBroj)
 
         binding.gumbAzuriraj.setOnClickListener {
-            azurirajItem()
+            val oglasnik = getOglasnik()
+            val oglasnikMap = getNewOglasnikMap()
+            azurirajItem(oglasnik,oglasnikMap)
+            val action = UpdateDeleteOglasnikDirections.actionUpdateDeleteOglasnikToOglasnikNavDrawer()
+            findNavController().navigate(action)
         }
 
         binding.gumbObrisi.setOnClickListener {
             val oglasnik = getOglasnik()
             deleteItem(oglasnik)
-            val action = UpdateDeleteOglasnikDirections.actionUpdateDeleteOglasnikToOglasnikNavDrawer()
+            val action =
+                UpdateDeleteOglasnikDirections.actionUpdateDeleteOglasnikToOglasnikNavDrawer()
             findNavController().navigate(action)
         }
 
     }
 
-    private fun getOglasnik(): OglasnikTable{
-        val naslov = binding.naslov.text.toString()
-        val clanak = binding.clanak.text.toString()
-        val cijena = binding.cijena.text.toString()
-        val lokacija = binding.lokacija.text.toString()
-        val broj = binding.broj.text.toString()
-        return OglasnikTable(clanak,naslov,cijena,lokacija,broj)
+    private fun getOglasnik(): OglasnikTable {
+        val naslov = binding.naslovStari.text.toString()
+        val clanak = binding.clanakStari.text.toString()
+        val cijena = binding.cijenaStari.text.toString()
+        val lokacija = binding.lokacijaStari.text.toString()
+        val broj = binding.brojStari.text.toString()
+        return OglasnikTable(clanak, naslov, cijena, lokacija, broj)
+    }
+
+    private fun getNewOglasnikMap(): Map<String, Any> {
+        val naslov = binding.naslovNovi.text.toString()
+        val clanak = binding.clanakNovi.text.toString()
+        val cijena = binding.cijenaNovi.text.toString()
+        val lokacija = binding.lokacijaNovi.text.toString()
+        val broj = binding.brojNovi.text.toString()
+        val map = mutableMapOf<String, Any>()
+        if (naslov.isNotEmpty()) {
+            map["oglasnikNaslov"] = naslov
+        }
+        if (clanak.isNotEmpty()) {
+            map["oglasnikClanak"] = clanak
+        }
+        if (cijena.isNotEmpty()) {
+            map["oglasnikCijena"] = cijena
+        }
+        if (clanak.isNotEmpty()) {
+            map["oglasnikLokacija"] = lokacija
+        }
+        if (clanak.isNotEmpty()) {
+            map["oglasnikBroj"] = broj
+        }
+        return map
     }
 
     private fun deleteItem(oglasnik: OglasnikTable) = CoroutineScope(Dispatchers.IO).launch {
@@ -86,7 +117,33 @@ class UpdateDeleteOglasnik: Fragment(R.layout.update_delete_oglasnik) {
         }
     }
 
-    private fun azurirajItem() {
-
-    }
+    private fun azurirajItem(oglasnik: OglasnikTable, oglasnikMap: Map<String, Any>) =
+        CoroutineScope(Dispatchers.IO).launch {
+            val query = collectionRef
+                .whereEqualTo("oglasnikClanak", oglasnik.oglasnikClanak)
+                .whereEqualTo("oglasnikNaslov", oglasnik.oglasnikNaslov)
+                .whereEqualTo("oglasnikCijena", oglasnik.oglasnikCijena)
+                .whereEqualTo("oglasnikLokacija", oglasnik.oglasnikLokacija)
+                .whereEqualTo("oglasnikBroj", oglasnik.oglasnikBroj)
+                .get()
+                .await()
+            if (query.documents.isNotEmpty()) {
+                for (document in query) {
+                    try {
+                        collectionRef.document(document.id).set(
+                            oglasnikMap,
+                            SetOptions.merge()
+                        ).await()
+                    } catch (e: Exception) {
+                        withContext(Dispatchers.Main) {
+                            Toast.makeText(requireContext(), e.message, Toast.LENGTH_LONG).show()
+                        }
+                    }
+                }
+            } else {
+                withContext(Dispatchers.Main) {
+                    Toast.makeText(requireContext(), "Neuspješno", Toast.LENGTH_LONG).show()
+                }
+            }
+        }
 }
