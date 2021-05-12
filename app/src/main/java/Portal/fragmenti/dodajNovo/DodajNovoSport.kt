ccc675c131ee5e1ceda526c7e74ea271d4710341
Dominik277 <dominikdoric277@gmail.com>
2021-05-12 23:26:13 +0200

    Implementirana logika do kraja za dohvacanje slike iz galerije.

--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoSport.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoSport.kt
@@ -4,11 +4,14 @@ import Portal.a257.R
 import Portal.a257.databinding.DodajNovoSportFragmentBinding
 import Portal.model.SportTable
 import android.Manifest
+import android.app.Activity
+import android.content.Intent
 import android.content.pm.PackageManager
 import android.os.Build
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat.checkSelfPermission
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.firestore.ktx.firestore
@@ -39,31 +42,52 @@ class DodajNovoSport : Fragment(R.layout.dodaj_novo_sport_fragment) {
         }
 
         binding.gumbOdaberiSLikuSport.setOnClickListener {
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M){
-                if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
-                        PackageManager.PERMISSION_DENIED){
+            if (checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) ==
+                PackageManager.PERMISSION_DENIED
+            ) {
                 //permission denied
-                    val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
-                    requestPermissions(permissions, PERMISSION_CODE)
-                }else{
-                        //permission already granted
-                    odaberiSliku()
-                }
-            }
-            else{
-                //system OS is < Marshmallow
+                val permissions = arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE)
+                requestPermissions(permissions, PERMISSION_CODE)
+            } else {
+                //permission already granted
                 odaberiSliku()
             }
         }
     }
 
     private fun odaberiSliku() {
-
+        val intent = Intent(Intent.ACTION_PICK)
+        intent.type = "image/*"
+        startActivityForResult(intent, IMAGE_PICK_CODE)
     }
 
-    companion object{
+    companion object {
         private val IMAGE_PICK_CODE = 100
         private val PERMISSION_CODE = 1001
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        when(requestCode){
+            PERMISSION_CODE -> {
+                if (grantResults.size > 0 && grantResults[0] ==
+                        PackageManager.PERMISSION_GRANTED){
+                    odaberiSliku()
+                }
+                else{
+                    Toast.makeText(requireContext(),"Permission denied",Toast.LENGTH_LONG).show()
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (resultCode == Activity.RESULT_OK && requestCode == IMAGE_PICK_CODE){
+            binding.imageViewSport.setImageURI(data?.data)
+        }
     }
 
     private fun savePerson(sport: SportTable) = CoroutineScope(Dispatchers.IO).launch {
