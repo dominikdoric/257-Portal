3a01701571221f5b36c7bd5af182824a677366cb
Dominik277 <dominikdoric277@gmail.com>
2021-05-15 13:14:41 +0200

    Dodano par sitnica.

--- app/src/main/java/Portal/fragmenti/fragmenti/VrijemePokusFragment.kt
+++ app/src/main/java/Portal/fragmenti/fragmenti/VrijemePokusFragment.kt
@@ -1,18 +1,17 @@
 package Portal.fragmenti.fragmenti
 
 import Portal.a257.R
-import Portal.a257.databinding.KartaFragmentBinding
+import Portal.a257.databinding.VrijemePokusFragmentBinding
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 
-class KartaFragment: Fragment(R.layout.karta_fragment) {
+class VrijemePokusFragment: Fragment(R.layout.vrijeme_pokus_fragment) {
 
-    private lateinit var binding: KartaFragmentBinding
+    private lateinit var binding: VrijemePokusFragmentBinding
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        binding = KartaFragmentBinding.bind(view)
+        binding = VrijemePokusFragmentBinding.bind(view)
     }
-
 }
