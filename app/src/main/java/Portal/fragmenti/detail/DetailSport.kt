05b0dd4cef442b6bf38c213f49d55a5d64b1f19d
Dominik277 <dominikdoric277@gmail.com>
2021-05-16 13:39:42 +0200

    Dodano jedno polje za URI u model klasu za sport.

--- app/src/main/java/Portal/fragmenti/detail/DetailSport.kt
+++ app/src/main/java/Portal/fragmenti/detail/DetailSport.kt
@@ -6,15 +6,24 @@ import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.navArgs
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.ktx.storage
 
 class DetailSport: Fragment(R.layout.detail_sport) {
 
     private lateinit var binding:DetailSportBinding
     private val args by navArgs<DetailSportArgs>()
+    private lateinit var storage: FirebaseStorage
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         binding = DetailSportBinding.bind(view)
+        storage = Firebase.storage
+        val storageRef = storage.reference
+        var imagesRef: StorageReference? = storageRef.child("images")
+        var spaceRef = storageRef.child("images/myImage.jpg")
 
         binding.naslov.text = args.sportArgs.naslov
         binding.clanak.text = args.sportArgs.clanak
--- app/src/main/java/Portal/model/SportTable.kt
+++ app/src/main/java/Portal/model/SportTable.kt
@@ -1,11 +1,11 @@
 package Portal.model
 
-import android.os.Parcel
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
 data class SportTable(
     var naslov: String? = "",
-    var clanak: String? = ""
+    var clanak: String? = "",
+    var sportURI: String = ""
 ) : Parcelable
