2f2015b099725bd4ac5f566f84f730625a87ce2b
Dominik277 <dominikdoric277@gmail.com>
2021-05-31 19:38:25 +0200

    Dodana logika za to da splash screen bude preko cijelog zaslona.

--- app/src/main/java/Portal/SplashActivity.kt
+++ app/src/main/java/Portal/SplashActivity.kt
@@ -1,12 +1,30 @@
 package Portal
 
 import Portal.a257.R
+import Portal.a257.databinding.ActivitySplashBinding
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
 
 class SplashActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivitySplashBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R){
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        }else{
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
