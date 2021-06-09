e18a6c662d4afe55c009e6c55379a1a2f8f54ca8
Dominik277 <dominikdoric277@gmail.com>
2021-06-10 00:15:24 +0200

    Dodana logika koja prikazuje poruku u alert dialog-u.

--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoObavijesti.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoObavijesti.kt
@@ -5,12 +5,19 @@ import Portal.a257.databinding.DialogCustomImageSelectionBinding
 import Portal.a257.databinding.DodajNovoObavijestiFragmentBinding
 import Portal.model.ObavijestiTable
 import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
 import android.app.Dialog
+import android.content.ActivityNotFoundException
 import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
 import android.provider.MediaStore
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.firestore.ktx.firestore
@@ -158,6 +165,59 @@ class DodajNovoObavijesti : Fragment(R.layout.dodaj_novo_obavijesti_fragment),Vi
 
         dialog.setContentView(binding.root)
         dialog.show()
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
+                data?.let {
+                    val thumbnail: Bitmap = data.extras?.get("data") as Bitmap
+                    binding.imageViewObavijesti.setImageBitmap(thumbnail)
+
+                    binding.addImageObavijesti.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.ic_edit
+                        )
+                    )
+                }
+            }
+        }
+        if (requestCode == GALLERY) {
+            data?.let {
+                val selectedPhotoUri = data.data
+                binding.imageViewObavijesti.setImageURI(selectedPhotoUri)
+
+                binding.addImageObavijesti.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_edit
+                    )
+                )
+            }
+        }
+    }
+
+    private fun showRationaleDialogForPermissions() {
+        AlertDialog.Builder(requireContext())
+            .setMessage(
+                "Izgleda da ste isključili dozvolu potrebnu za ovaj dio. Ona ponovno" +
+                        " može biti omogućena u postavkama aplikacije"
+            )
+            .setPositiveButton("Go to settings") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", "packageName", null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
     }
 
 }
