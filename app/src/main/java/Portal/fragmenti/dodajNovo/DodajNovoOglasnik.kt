11e812cd9d92ea802567162f91aea12f860fc2d7
Dominik277 <dominikdoric277@gmail.com>
2021-06-10 00:22:49 +0200

    Dodano par stvari.

--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoOglasnik.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoOglasnik.kt
@@ -3,9 +3,17 @@ package Portal.fragmenti.dodajNovo
 import Portal.a257.R
 import Portal.a257.databinding.DodajNovoOglasnikFragmentBinding
 import Portal.model.OglasnikTable
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.firestore.ktx.firestore
@@ -20,10 +28,15 @@ import java.lang.Exception
 import java.lang.StringBuilder
 
 
-class DodajNovoOglasnik : Fragment(R.layout.dodaj_novo_oglasnik_fragment) {
+class DodajNovoOglasnik : Fragment(R.layout.dodaj_novo_oglasnik_fragment), View.OnClickListener {
 
     private val personCollectionRef = Firebase.firestore.collection("oglasnik")
     private lateinit var binding: DodajNovoOglasnikFragmentBinding
+
+    companion object {
+        private const val CAMERA = 1
+        private const val GALLERY = 2
+    }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -68,4 +81,69 @@ class DodajNovoOglasnik : Fragment(R.layout.dodaj_novo_oglasnik_fragment) {
             }
         }
     }
+
+    override fun onClick(v: View?) {
+        if (v != null) {
+            when (v.id) {
+                R.id.addImageOglasnik -> {
+                    customImageSelectionDialog()
+                    return
+                }
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
+                data?.let {
+                    val thumbnail: Bitmap = data.extras?.get("data") as Bitmap
+                    binding.imageViewOglasnik.setImageBitmap(thumbnail)
+
+                    binding.addImageOglasnik.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.ic_edit
+                        )
+                    )
+                }
+            }
+        }
+        if (requestCode == GALLERY) {
+            data?.let {
+                val selectedPhotoUri = data.data
+                binding.imageViewOglasnik.setImageURI(selectedPhotoUri)
+
+                binding.addImageOglasnik.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_edit
+                    )
+                )
+            }
+        }
+    }
+
+    private fun showRationaleDialogForPermissions() {
+        AlertDialog.Builder(requireContext())
+            .setMessage(
+                "Izgleda da ste isključili dozvolu potrebnu za ovaj dio. Ona ponovno" +
+                        " može biti omogućena u postavkama aplikacije"
+            )
+            .setPositiveButton("Go to settings") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", "packageName", null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
 }
