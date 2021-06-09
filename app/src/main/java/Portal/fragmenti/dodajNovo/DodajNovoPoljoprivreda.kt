dbe07663ae32432a47761022e10303914cf809d4
Dominik277 <dominikdoric277@gmail.com>
2021-06-10 00:02:08 +0200

    Dodane funkcije koje prikazuju tekst nakon sto korisnik odbije dodijeliti aplikaciji dopustenje.

--- app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoPoljoprivreda.kt
+++ app/src/main/java/Portal/fragmenti/dodajNovo/DodajNovoPoljoprivreda.kt
@@ -3,9 +3,17 @@ package Portal.fragmenti.dodajNovo
 import Portal.a257.R
 import Portal.a257.databinding.DodajNovoPoljoprivredaFragmentBinding
 import Portal.model.PoljoprivredaTable
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ActivityNotFoundException
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import android.view.View
 import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.google.firebase.firestore.ktx.firestore
@@ -29,7 +37,7 @@ class DodajNovoPoljoprivreda: Fragment(R.layout.dodaj_novo_poljoprivreda_fragmen
         super.onViewCreated(view, savedInstanceState)
         binding = DodajNovoPoljoprivredaFragmentBinding.bind(view)
 
-        binding.gumbSpremiPoljoprivredu.setOnClickListener {
+        binding.gumbSpremiPoljoprivreda.setOnClickListener {
             val naslov = binding.naslov.text.toString()
             val clanak = binding.clanak.text.toString()
             val poljoprivreda = PoljoprivredaTable(naslov, clanak)
@@ -58,4 +66,58 @@ class DodajNovoPoljoprivreda: Fragment(R.layout.dodaj_novo_poljoprivreda_fragmen
             }
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (resultCode == Activity.RESULT_OK) {
+            if (requestCode == CAMERA) {
+                data?.let {
+                    val thumbnail: Bitmap = data.extras?.get("data") as Bitmap
+                    binding.imageViewPoljoprivreda.setImageBitmap(thumbnail)
+
+                    binding.addImagePoljoprivreda.setImageDrawable(
+                        ContextCompat.getDrawable(
+                            requireContext(),
+                            R.drawable.ic_edit
+                        )
+                    )
+                }
+            }
+        }
+        if (requestCode == GALLERY) {
+            data?.let {
+                val selectedPhotoUri = data.data
+                binding.imageViewPoljoprivreda.setImageURI(selectedPhotoUri)
+
+                binding.addImagePoljoprivreda.setImageDrawable(
+                    ContextCompat.getDrawable(
+                        requireContext(),
+                        R.drawable.ic_edit
+                    )
+                )
+            }
+        }
+    }
+
+    private fun showRationaleDialogForPermissions() {
+        AlertDialog.Builder(requireContext())
+            .setMessage(
+                "Izgleda da ste isključili dozvolu potrebnu za ovaj dio. Ona ponovno" +
+                        " može biti omogućena u postavkama aplikacije"
+            )
+            .setPositiveButton("Go to settings") { _, _ ->
+                try {
+                    val intent = Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS)
+                    val uri = Uri.fromParts("package", "packageName", null)
+                    intent.data = uri
+                    startActivity(intent)
+                } catch (e: ActivityNotFoundException) {
+                    e.printStackTrace()
+                }
+            }
+            .setNegativeButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }.show()
+    }
+
 }
